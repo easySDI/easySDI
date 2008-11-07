fefa83c464a82923a2655d3e6eb2641b8d911e59
rmichaelis <rmichaelis@f71f33a3-9987-4f9c-9609-a93d63a967da>
2008-11-07 13:18:10 +0000

    Move Catalog component from shop to catalog

---
+++ catalog/joomla/1.5/trunk/com_easysdi_catalog/com_easysdi_catalog/easysdi_catalog.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * EasySDI, a solution to implement easily any spatial data infrastructure
+ * Copyright (C) 2008 DEPTH SA, Chemin d’Arche 40b, CH-1870 Monthey, easysdi@depth.ch 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or 
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html. 
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+jimport("joomla.html.pagination");
+
+jimport("joomla.html.pane");
+
+
+include_once(JPATH_LIBRARIES.DS.'joomla'.DS.'database'.DS.'table'.DS.'user.php');
+require_once(JPATH_COMPONENT.DS.'core'.DS.'catalog.site.easysdi.php');
+require_once(JPATH_COMPONENT.DS.'core'.DS.'catalog.site.easysdi.html.php');
+require_once(JPATH_COMPONENT.DS.'core'.DS.'catalog.site.easysdi.class.php');
+require_once(JPATH_COMPONENT_ADMINISTRATOR.DS.'core'.DS.'geoMetadata.php');
+
+$language=&JFactory::getLanguage();
+$language->load('com_easysdi_catalog');
+
+?>
+<?php
+$option = JRequest::getVar('option');
+$task = JRequest::getVar('task');
+
+
+switch($task){
+	default:
+	case "listCatalogContent":			
+		SITE_catalog::listCatalogContent();
+		break;		
+}
+ ?>
