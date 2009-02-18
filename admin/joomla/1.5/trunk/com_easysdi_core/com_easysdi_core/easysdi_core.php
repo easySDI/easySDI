6a2fd89dab71bb834e1536114b8c94447dd41af0
rmichaelis <rmichaelis@f71f33a3-9987-4f9c-9609-a93d63a967da>
2009-02-18 21:10:05 +0000

    GeoMetadata

--- admin/joomla/1.5/trunk/com_easysdi_core/com_easysdi_core/easysdi_core.php
+++ admin/joomla/1.5/trunk/com_easysdi_core/com_easysdi_core/easysdi_core.php
@@ -24,7 +24,7 @@ jimport("joomla.html.pane");
 require_once(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_easysdi_core'.DS.'common'.DS.'easysdi.config.php');
 require_once(JPATH_COMPONENT.DS.'core'.DS.'partner.site.easysdi.php');
 require_once(JPATH_COMPONENT.DS.'core'.DS.'partner.site.easysdi.html.php');
-require_once(JPATH_COMPONENT.DS.'core'.DS.'partner.site.easysdi.class.php');
+require_once(JPATH_BASE.DS.'components'.DS.'com_easysdi_core'.DS.'core'.DS.'partner.site.easysdi.class.php');
 require_once(JPATH_COMPONENT.DS.'js'.DS.'partner.site.easysdi.php');
 require_once(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_easysdi_core'.DS.'common'.DS.'easysdi.usermanager.class.php');
 
