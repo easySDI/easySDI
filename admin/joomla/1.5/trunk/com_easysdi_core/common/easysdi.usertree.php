d1e5310011a41295970d51927061c0f02e264935
rbaud <rbaud@f71f33a3-9987-4f9c-9609-a93d63a967da>
2009-06-29 13:16:45 +0000

    added slashes for javascript output

--- admin/joomla/1.5/trunk/com_easysdi_core/common/easysdi.usertree.php
+++ admin/joomla/1.5/trunk/com_easysdi_core/common/easysdi.usertree.php
@@ -40,7 +40,7 @@ class userTree
 			<script type="text/javascript">
 				d = new dTree('d', '<?php echo JURI::root().'templates/easysdi/';  ?>');
 				
-				d.add(0,-1,'<?php echo $rootUser->name;  ?>');
+				d.add(0,-1,'<?php echo addslashes($rootUser->name);  ?>');
 				
 				<?php 
 				$query = "SELECT * FROM #__easysdi_community_partner up, #__users u where up.partner_id != up.parent_id  AND up.parent_id = '$rootUser->partner_id' AND up.user_id = u.id ORDER BY partner_id";						
@@ -79,13 +79,13 @@ class userTree
 				}
 				
 			?>
-				d.add(<?php echo $i?>,<?php echo $parentNodeId?>,'<?php echo $childUser->name;  ?>','<?php JUri::base(true);?>index.php?search=<?php echo JRequest::getVar('search'); ?>&type=3&affiliate_id=<?php echo $childUser->user_id; ?>&return=<?php echo $return; ?>&option=com_easysdi_core&task=editAffiliateById');
+				d.add(<?php echo $i?>,<?php echo $parentNodeId?>,'<?php echo addslashes($childUser->name);  ?>','<?php JUri::base(true);?>index.php?search=<?php echo JRequest::getVar('search'); ?>&type=3&affiliate_id=<?php echo $childUser->user_id; ?>&return=<?php echo $return; ?>&option=com_easysdi_core&task=editAffiliateById');
 			<?php
 			}
 			else
 			{
 				?>
-				d.add(<?php echo $i?>,<?php echo $parentNodeId?>,'<?php echo $childUser->name;  ?>','<?php JUri::base(true);?>index.php?cid[]=<?php echo $childUser->partner_id; ?>&option=com_easysdi_core&task=editAffiliatePartner');
+				d.add(<?php echo $i?>,<?php echo $parentNodeId?>,'<?php echo addslashes($childUser->name);  ?>','<?php JUri::base(true);?>index.php?cid[]=<?php echo $childUser->partner_id; ?>&option=com_easysdi_core&task=editAffiliatePartner');
 				<?php
 			}
 			$query = "SELECT * FROM #__easysdi_community_partner up, #__users u where up.partner_id != up.parent_id  AND up.parent_id = '$childUser->partner_id' AND up.user_id = u.id ORDER BY partner_id";						
