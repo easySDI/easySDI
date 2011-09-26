83c85d084953acba43ea8cb80c1d0cabb2155f8d
kghoorbin <kghoorbin@f71f33a3-9987-4f9c-9609-a93d63a967da>
2011-09-26 10:55:51 +0000

    wrong copy

--- branches/crigeos/sandbox_kg/template_crigeos_js_verdant2_ksb/sandbox_kg/js_verdant2/js/equalcolumns.js
+++
@@ -1,9 +0,0 @@
-function equal_columns(element) {
-	var columns = $$(element);
-	var max_height = 0;
-
-	columns.each(function(item) 
-	{ max_height = Math.max(max_height, item.getSize().size.y ); });
-
-	columns.setStyle('height', max_height);
-}	
