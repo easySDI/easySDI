f625c37162fdc3c6d6072e031890b921ec0018ab
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2011-12-20 16:05:01 +0000

    #5663 : thesaurus bugs (front-end)

--- branches/crigeos/catalog/com_easysdi_catalog/core/view/metadata.site.easysdi.html.php
+++ branches/crigeos/catalog/com_easysdi_catalog/core/view/metadata.site.easysdi.html.php
@@ -51,7 +51,7 @@ JHTML::script('shBrushXml.js', 'administrator/components/com_easysdi_catalog/js/
 //JHTML::script('GemetClient-uncompressed.js', 'administrator/components/com_easysdi_catalog/js/');
 JHTML::script('thesaur.js', 'administrator/components/com_easysdi_catalog/js/');
 JHTML::script('HS.js', 'administrator/components/com_easysdi_catalog/js/');
-JHTML::script('translations.js', 'administrator/components/com_easysdi_catalog/js/');
+//JHTML::script('translations.js', 'administrator/components/com_easysdi_catalog/js/');
 require_once(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_easysdi_core'.DS.'common'.DS.'easysdi.jsLoaderUtil.php');
 $jsLoader =JSLOADER_UTIL::getInstance();
 JHTML::script('SingleFile.js', $jsLoader->getPath("map","openlayers", "/lib/OpenLayers/"));
