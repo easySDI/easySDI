0c29a9daf91aba4d29a0451ca9314eff154d0401
bmagoni <bmagoni@f71f33a3-9987-4f9c-9609-a93d63a967da>
2012-01-19 15:11:56 +0000

    refactoring SVN branches

--- branches/crigeos/map/components/resource/map_context.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE #__easysdi_map_context(
-id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-user_id INT NOT NULL,
-WMC_text TEXT
-);
--- branches/crigeos/template_crigeos_js_verdant2/js_verdant2/html/mod_newsflash/vert.php
+++
@@ -1,9 +0,0 @@
-<?php defined('_JEXEC') or die('Restricted access'); ?>
-
-<?php for ($i = 0, $n = count($list); $i < $n; $i ++) :
-	modNewsFlashHelper::renderItem($list[$i], $params, $access);
-	if ($n > 1 && (($i < $n - 1) || $params->get('showLastSeparator'))) : ?>
-
-
- 	<?php endif; ?>
-<?php endfor; ?>
