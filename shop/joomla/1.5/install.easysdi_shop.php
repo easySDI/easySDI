66947657f7d52b08861b289302a77e711312109e
Hélène Van Hoecke <2844416+hvanhoecke@users.noreply.github.com>
2013-01-11 16:42:32 +0000

    #579

--- shop/joomla/1.5/install.easysdi_shop.php
+++ shop/joomla/1.5/install.easysdi_shop.php
@@ -1144,6 +1144,26 @@ function com_install(){
 			$mainframe->enqueueMessage($db->getErrorMsg(),"ERROR");
 		}
 	}
+	if($version == "2.2.0.1")
+	{
+		// Update component version
+		$version="2.2.1";
+		$query="UPDATE #__sdi_list_module SET currentversion ='".$version."' WHERE code='SHOP'";
+		$db->setQuery( $query);
+		if (!$db->query()){
+			$mainframe->enqueueMessage($db->getErrorMsg(),"ERROR");
+		}
+	}
+	if($version == "2.2.1")
+	{
+		// Update component version
+		$version="2.2.2";
+		$query="UPDATE #__sdi_list_module SET currentversion ='".$version."' WHERE code='SHOP'";
+		$db->setQuery( $query);
+		if (!$db->query()){
+			$mainframe->enqueueMessage($db->getErrorMsg(),"ERROR");
+		}
+	}
 	 
 	$query = "DELETE FROM #__components where `option`= 'com_easysdi_shop' ";
 	$db->setQuery( $query);
