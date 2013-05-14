658a1b575e534902bc42a0facfe667b363568367
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2013-05-14 07:59:19 +0000

    Ajout du contenu de src à com_easysdi_core, sauf le dossier js_minifier

---
+++ branches/depth/running/v330/sandbox/mb/joomla/com_easysdi_core/src/administrator/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,25 @@
+SET foreign_key_checks = 0;
+
+DROP TABLE IF EXISTS `#__sdi_sys_unit`;
+DROP TABLE IF EXISTS `#__sdi_sys_role`;
+DROP TABLE IF EXISTS `#__sdi_sys_country`;
+DROP TABLE IF EXISTS `#__sdi_sys_versiontype`;
+DROP TABLE IF EXISTS `#__sdi_sys_accessscope`;
+DROP TABLE IF EXISTS `#__sdi_sys_metadatastate`;
+DROP TABLE IF EXISTS `#__sdi_sys_spatialoperator`;
+DROP TABLE IF EXISTS `#__sdi_sys_serviceconnector`;
+DROP TABLE IF EXISTS `#__sdi_sys_serviceversion`;
+DROP TABLE IF EXISTS `#__sdi_sys_servicecompliance`;
+DROP TABLE IF EXISTS `#__sdi_sys_serviceoperation`;
+DROP TABLE IF EXISTS `#__sdi_sys_operationcompliance`;
+DROP TABLE IF EXISTS `#__sdi_sys_authenticationlevel`;
+DROP TABLE IF EXISTS `#__sdi_sys_authenticationconnector`;
+DROP TABLE IF EXISTS `#__sdi_sys_logroll`;
+DROP TABLE IF EXISTS `#__sdi_sys_loglevel`;
+DROP TABLE IF EXISTS `#__sdi_sys_exceptionlevel`;
+DROP TABLE IF EXISTS `#__sdi_sys_proxytype`;
+DROP TABLE IF EXISTS `#__sdi_sys_servicecon_authenticationcon`;
+DROP TABLE IF EXISTS `#__sdi_sys_servicescope`;
+DROP TABLE IF EXISTS `#__sdi_sys_metadataversion`;
+
+SET foreign_key_checks = 1;
