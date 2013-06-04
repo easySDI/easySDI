1d56ca4f757f40ab4600302f5ae945997e7168dc
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2013-06-04 15:28:39 +0000

    Remise de la version initiale des requetes WFS

--- sandboxes/depth/running/v330/sandbox/hvh/plugin_content/content/easysdi.php
+++ sandboxes/depth/running/v330/sandbox/hvh/plugin_content/content/easysdi.php
@@ -1,7 +1,7 @@
 <?php
 /**
-* @version     3.3.0
-* @package     plg_content_easysdi
+* @version     4.0.0
+* @package     plg_easysdi_content
 * @copyright   Copyright (C) 2013. All rights reserved.
 * @license     GNU General Public License version 3 or later; see LICENSE.txt
 * @author      EasySDI Community <contact@easysdi.org> - http://www.easysdi.org
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 /**
  * 
  *
- * @package     plg_content_easysdi
+ * @package     plg_easysdi_content
  * @subpackage  
  * @since       3.3.0
  */
