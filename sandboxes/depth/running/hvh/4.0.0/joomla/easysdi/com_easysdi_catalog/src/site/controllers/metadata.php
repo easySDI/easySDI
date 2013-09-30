6dbf201d9c00f5b3fb3217c4bfb4dbbca52f1e2f
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2013-09-30 08:14:31 +0000

    Merge branche 4-alpha-7

--- sandboxes/depth/running/hvh/4.0.0/joomla/easysdi/com_easysdi_catalog/src/site/controllers/metadata.php
+++ sandboxes/depth/running/hvh/4.0.0/joomla/easysdi/com_easysdi_catalog/src/site/controllers/metadata.php
@@ -10,7 +10,21 @@
 // No direct access
 defined('_JEXEC') or die;
 
+JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_easysdi_catalog/tables');
+
 require_once JPATH_COMPONENT . '/controller.php';
+
+require_once JPATH_BASE . '/components/com_easysdi_catalog/libraries/easysdi/FormHtmlGenerator.php';
+require_once JPATH_BASE . '/components/com_easysdi_catalog/libraries/easysdi/DomCswCreator.php';
+
+require_once JPATH_BASE . '/components/com_easysdi_catalog/libraries/easysdi/po/SdiRelation.php';
+require_once JPATH_BASE . '/components/com_easysdi_catalog/libraries/easysdi/po/SdiClass.php';
+require_once JPATH_BASE . '/components/com_easysdi_catalog/libraries/easysdi/po/SdiAttribute.php';
+require_once JPATH_BASE . '/components/com_easysdi_catalog/libraries/easysdi/po/SdiNamespace.php';
+require_once JPATH_BASE . '/components/com_easysdi_catalog/libraries/easysdi/po/SdiStereotype.php';
+require_once JPATH_BASE . '/components/com_easysdi_catalog/libraries/easysdi/po/SdiResourcetype.php';
+
+require_once JPATH_ADMINISTRATOR .'/components/com_easysdi_core/libraries/easysdi/catalog/sdimetadata.php';
 
 /**
  * Metadata controller class.
@@ -56,85 +70,118 @@ class Easysdi_catalogControllerMetadata extends Easysdi_catalogController {
      * @since	1.6
      */
     public function save() {
-        
-        // Initialise variables.
-        $app = JFactory::getApplication();
-        $model = $this->getModel('Metadata', 'Easysdi_catalogModel');
-
-        // Get the user data.
+        $relations = $this->getRelations();
         $data = JFactory::getApplication()->input->get('jform', array(), 'array');
-
-        // Validate the posted data.
-        $form = $model->getForm();
         
-        if (!$form) {
-            JError::raiseError(500, $model->getError());
-            return false;
-        }
-
-        // Validate the posted data.
-        $data = $model->validate($form, $data);
-
-        // Check for errors.
-        if ($data === false) {
-            // Get the validation messages.
-            $errors = $model->getErrors();
-
-            // Push up to three validation messages out to the user.
-            for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++) {
-                if ($errors[$i] instanceof Exception) {
-                    $app->enqueueMessage($errors[$i]->getMessage(), 'warning');
-                } else {
-                    $app->enqueueMessage($errors[$i], 'warning');
+        $smda = new sdiMetadata($data['id']);
+        
+        foreach ($relations as $key => $rel) {
+            if ($rel->childtype_id == SdiRelation::$ATTRIBUT) {
+                if (array_key_exists($key, $data)) {
+                    $rel->getAttribut_child()->value = $data[$key];
                 }
             }
-
-            // Save the data in the session.
-            $app->setUserState('com_easysdi_catalog.edit.metadata.data', JRequest::getVar('jform'), array());
-
-            // Redirect back to the edit screen.
-            $id = (int) $app->getUserState('com_easysdi_catalog.edit.metadata.id');
-            $this->setRedirect(JRoute::_('index.php?option=com_easysdi_catalog&view=metadata&layout=edit&id=' . $id, false));
-            return false;
         }
+        
+        $dcc = new DomCswCreator($relations, $data['id'], $data['guid']);
 
-        // Attempt to save the data.
-        $return = $model->save($data);
+        
+        $smda->update($dcc->getCsw());
+    
 
-        // Check for errors.
-        if ($return === false) {
-            // Save the data in the session.
-            $app->setUserState('com_easysdi_catalog.edit.metadata.data', $data);
-
-            // Redirect back to the edit screen.
-            $id = (int) $app->getUserState('com_easysdi_catalog.edit.metadata.id');
-            $this->setMessage(JText::sprintf('Save failed', $model->getError()), 'warning');
-            $this->setRedirect(JRoute::_('index.php?option=com_easysdi_catalog&view=metadata&layout=edit&id=' . $id, false));
-            return false;
-        }
-
-
-        // Check in the profile.
-        if ($return) {
-            $model->checkin($return);
-        }
-
-        // Clear the profile id from the session.
-        $app->setUserState('com_easysdi_catalog.edit.metadata.id', null);
-
-        // Redirect to the list screen.
-        $this->setMessage(JText::_('COM_EASYSDI_CATALOG_ITEM_SAVED_SUCCESSFULLY'));
-        $this->setRedirect(JRoute::_('index.php?option=com_easysdi_core&view=resources', false));
-
-        // Flush the data from the session.
-        $app->setUserState('com_easysdi_catalog.edit.metadata.data', null);
+//        // Initialise variables.
+//        $app = JFactory::getApplication();
+//        $model = $this->getModel('Metadata', 'Easysdi_catalogModel');
+//
+//        // Get the user data.
+//        $data = JFactory::getApplication()->input->get('jform', array(), 'array');
+//
+//        // Validate the posted data.
+//        $form = $model->getForm();
+//        
+//        if (!$form) {
+//            JError::raiseError(500, $model->getError());
+//            return false;
+//        }
+//
+//        // Validate the posted data.
+//        $data = $model->validate($form, $data);
+//
+//        // Check for errors.
+//        if ($data === false) {
+//            // Get the validation messages.
+//            $errors = $model->getErrors();
+//
+//            // Push up to three validation messages out to the user.
+//            for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++) {
+//                if ($errors[$i] instanceof Exception) {
+//                    $app->enqueueMessage($errors[$i]->getMessage(), 'warning');
+//                } else {
+//                    $app->enqueueMessage($errors[$i], 'warning');
+//                }
+//            }
+//
+//            // Save the data in the session.
+//            $app->setUserState('com_easysdi_catalog.edit.metadata.data', JRequest::getVar('jform'), array());
+//
+//            // Redirect back to the edit screen.
+//            $id = (int) $app->getUserState('com_easysdi_catalog.edit.metadata.id');
+//            $this->setRedirect(JRoute::_('index.php?option=com_easysdi_catalog&view=metadata&layout=edit&id=' . $id, false));
+//            return false;
+//        }
+//
+//        // Attempt to save the data.
+//        $return = $model->save($data);
+//
+//        // Check for errors.
+//        if ($return === false) {
+//            // Save the data in the session.
+//            $app->setUserState('com_easysdi_catalog.edit.metadata.data', $data);
+//
+//            // Redirect back to the edit screen.
+//            $id = (int) $app->getUserState('com_easysdi_catalog.edit.metadata.id');
+//            $this->setMessage(JText::sprintf('Save failed', $model->getError()), 'warning');
+//            $this->setRedirect(JRoute::_('index.php?option=com_easysdi_catalog&view=metadata&layout=edit&id=' . $id, false));
+//            return false;
+//        }
+//
+//
+//        // Check in the profile.
+//        if ($return) {
+//            $model->checkin($return);
+//        }
+//
+//        // Clear the profile id from the session.
+//        $app->setUserState('com_easysdi_catalog.edit.metadata.id', null);
+//
+//        // Redirect to the list screen.
+//        $this->setMessage(JText::_('COM_EASYSDI_CATALOG_ITEM_SAVED_SUCCESSFULLY'));
+//        $this->setRedirect(JRoute::_('index.php?option=com_easysdi_core&view=resources', false));
+//
+//        // Flush the data from the session.
+//        $app->setUserState('com_easysdi_catalog.edit.metadata.data', null);
     }
 
     function cancel() {
-        
+
         $this->setRedirect(JRoute::_('index.php?option=com_easysdi_core&view=resources', false));
     }
 
-   
+    protected function buildForm() {
+        $fhg = new FormHtmlGenerator($this->form);
+
+        $classTree = $this->classTree = $this->get('ClassTree');
+        return $fhg->buildForm($classTree);
+    }
+
+    /**
+     * 
+     * @return SdiRelation[]
+     */
+    private function getRelations() {
+        $session = JFactory::getSession();
+        $relations = unserialize($session->get('relations'));
+        return $relations;
+    }
 
 }
--- sandboxes/depth/running/hvh/4.0.0/joomla/easysdi/com_easysdi_catalog/src/site/views/metadata/tmpl/default.php
+++ sandboxes/depth/running/hvh/4.0.0/joomla/easysdi/com_easysdi_catalog/src/site/views/metadata/tmpl/default.php
@@ -12,107 +12,166 @@ defined('_JEXEC') or die;
 JHtml::_('behavior.keepalive');
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.formvalidation');
+JHtml::_('formbehavior.chosen', 'select');
 
 //Load admin language file
 $lang = JFactory::getLanguage();
 $lang->load('com_easysdi_catalog', JPATH_ADMINISTRATOR);
 ?>
 
-<!-- Styling for making front end forms look OK -->
-<!-- This should probably be moved to the template CSS file -->
 <style>
-    .front-end-edit ul {
-        padding: 0 !important;
+
+    .inner-fds{
+        padding-left:15px;
+        border-left: 1px solid #BDBDBD;
     }
-    .front-end-edit li {
-        list-style: none;
-        margin-bottom: 6px !important;
-    }
-    .front-end-edit label {
+
+    .collapse-btn, .neutral-btn{
+        cursor: pointer;
         margin-right: 10px;
-        display: block;
-        float: left;
-        width: 200px !important;
-    }
-    .front-end-edit .radio label {
-        float: none;
-    }
-    .front-end-edit .readonly {
-        border: none !important;
-        color: #666;
-    }    
-    .front-end-edit #editor-xtd-buttons {
-        height: 50px;
-        width: 600px;
-        float: left;
-    }
-    .front-end-edit .toggle-editor {
-        height: 50px;
-        width: 120px;
-        float: right;
     }
 
-    #jform_rules-lbl{
-        display:none;
+    .neutral-btn{
+        margin-right: 10px;
     }
 
-    #access-rules a:hover{
-        background:#f5f5f5 url('../images/slider_minus.png') right  top no-repeat;
-        color: #444;
+    .add-btn, .remove-btn{
+        cursor: pointer;
+        margin-left: 10px;
     }
 
-    fieldset.radio label{
-        width: 50px !important;
+    legend{
+        font-size: 12px;
     }
+
 </style>
+
 <script type="text/javascript">
-    function getScript(url,success) {
-        var script = document.createElement('script');
-        script.src = url;
-        var head = document.getElementsByTagName('head')[0],
-        done = false;
-        // Attach handlers for all browsers
-        script.onload = script.onreadystatechange = function() {
-            if (!done && (!this.readyState
-                || this.readyState == 'loaded'
-                || this.readyState == 'complete')) {
-                done = true;
-                success();
-                script.onload = script.onreadystatechange = null;
-                head.removeChild(script);
-            }
-        };
-        head.appendChild(script);
-    }
-    getScript('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',function() {
-        js = jQuery.noConflict();
-        js(document).ready(function(){
-            js('#form-metadata').submit(function(event){
-                 
-            }); 
-        
-            
-        });
-    });
+    js = jQuery.noConflict();
+
+    js('document').ready(function() {
+
+<?php foreach ($this->validators as $validator) { 
     
+     echo $validator;
+    
+} ?>
+
+
+        js('#btn_toogle_all').click(function() {
+            var btn = js(this);
+
+            if (btn.attr('action') == 'open') {
+                btn.text('Tout fermer');
+                js('.inner-fds').show();
+                js('.collapse-btn').attr({'src': '/joomla/administrator/components/com_easysdi_catalog/assets/images/collapse_top.png'});
+                btn.attr({'action': 'close'});
+            } else {
+                btn.text('Tout ouvrir');
+                js('.inner-fds').hide();
+                js('.collapse-btn').attr({'src': '/joomla/administrator/components/com_easysdi_catalog/assets/images/expand.png'});
+                btn.attr({'action': 'open'});
+            }
+
+
+        });
+
+    });
+
+    function collapse(id) {
+
+        var uuid = getUuid('collapse-btn-', id);
+        var current_div = js('#inner-fds-' + uuid);
+        var current_btn = js('#' + id);
+
+        current_div.toggle('fast', function() {
+            if (current_div.css('display') == 'none') {
+                current_btn.attr({'src': '/joomla/administrator/components/com_easysdi_catalog/assets/images/expand.png'});
+            } else {
+                current_btn.attr({'src': '/joomla/administrator/components/com_easysdi_catalog/assets/images/collapse_top.png'});
+            }
+        });
+    }
+
+    function addFieldset(id, idwi, lowerbound, upperbound) {
+        var uuid = getUuid('add-btn-', id);
+
+        js.get('http://localhost/joomla/index.php/ajax?uuid=' + uuid, function(data) {
+            js('.outer-fds-' + idwi).last().after(data);
+
+            var occurance = getOccuranceCount('.outer-fds-' + idwi);
+
+            if (upperbound > occurance) {
+                js('.add-btn-' + idwi).show();
+            }
+
+            if (occurance > lowerbound) {
+                js('.remove-btn-' + idwi).show();
+            }
+
+            if (upperbound == occurance) {
+                js('.add-btn-' + idwi).hide();
+            }
+        });
+
+
+    }
+
+    function removeFieldset(id, idwi, lowerbound, upperbound) {
+        var uuid = getUuid('remove-btn-', id);
+
+        var toRemove = js('#outer-fds-' + uuid);
+
+        toRemove.remove();
+
+        var occurance = getOccuranceCount('.outer-fds-' + idwi);
+
+        if (lowerbound == occurance) {
+            js('.remove-btn-' + idwi).hide();
+        }
+
+        if (upperbound > occurance) {
+            js('.add-btn-' + idwi).show();
+        }
+    }
+
+    /**
+     * 
+     * @param {string} prefix
+     * @param {string} string
+     * @returns {array}
+     */
+    function getUuid(prefix, string) {
+        string = string.replace(prefix, '');
+        return string;
+    }
+
+    function getOccuranceCount(className) {
+        var nbr = js(className).length;
+        return nbr - 1;
+    }
+
 </script>
 
 <div class="metadata-edit front-end-edit">
-    
+    <button id="btn_toogle_all" action="open" class="btn">Tout ouvrir</button>
     <h2><?php echo JText::_('COM_EASYSDI_CATALOGE_TITLE_EDIT_METADATA') . ' ' . $this->item->guid; ?></h2>
-        
-    <form id="form-metadata" action="<?php echo JRoute::_('index.php?option=com_easysdi_catalog&task=metadata.save'); ?>" method="post" class="form-validate" enctype="multipart/form-data">
-        <div>
-             <?php foreach ($this->form->getFieldset('hidden') as $field): ?>
-                    <?php echo $field->input; ?>
-            <?php endforeach; ?>
-        </div>
+
+    <form id="form-metadata" action="<?php echo JRoute::_('index.php?option=com_easysdi_catalog&task=metadata.save'); ?>" method="post" class="form-validate form-horizontal" enctype="multipart/form-data">
+        <?php foreach ($this->form->getFieldset('hidden') as $field): ?>
+            <?php echo $field->input; ?>
+        <?php endforeach; ?>
         <div class ="well">
-        <?php echo htmlspecialchars($this->item->csw); ?>
+
+            <?php //echo htmlspecialchars($this->item->csw);   ?>
+
+            <?php echo $this->formHtml; ?>
+
         </div>
+
         <div>
             <button type="submit" class="validate"><span><?php echo JText::_('JSUBMIT'); ?></span></button>
-            <?php echo JText::_('or'); ?>
+                    <?php echo JText::_('or'); ?>
             <a href="<?php echo JRoute::_('index.php?option=com_easysdi_catalog&task=metadata.cancel'); ?>" title="<?php echo JText::_('JCANCEL'); ?>"><?php echo JText::_('JCANCEL'); ?></a>
 
             <input type="hidden" name="option" value="com_easysdi_catalog" />
