3c1119bdbf8cf4dc9be5ed35ffa13ad4efdbf81a
yblatti <yblatti@f71f33a3-9987-4f9c-9609-a93d63a967da>
2013-10-29 13:46:31 +0000

    Base from 4.0.0-alpha-10a

---
+++ sandboxes/asit-vd/4.0.0-admhome/joomla/easysdi/com_easysdi_catalog/src/site/controllers/ajax.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * @version     4.0.0
+ * @package     com_easysdi_catalog
+ * @copyright   Copyright (C) 2013. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      EasySDI Community <contact@easysdi.org§> - http://www.easysdi.org
+ */
+// No direct access
+defined('_JEXEC') or die;
+
+JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_easysdi_catalog/tables');
+
+require_once JPATH_COMPONENT . '/controller.php';
+require_once JPATH_BASE . '/components/com_easysdi_catalog/libraries/easysdi/dao/SdiNamespaceDao.php';
+
+/**
+ * Metadata controller class.
+ */
+class Easysdi_catalogControllerAjax extends Easysdi_catalogController {
+
+    /**
+     * database
+     *
+     * @var JDatabaseDriver
+     */
+    private $db = null;
+
+    /**
+     *
+     * @var JSession 
+     */
+    private $session;
+
+    /**
+     *
+     * @var DOMDocument 
+     */
+    private $structure;
+
+    /**
+     *
+     * @var DOMXPath 
+     */
+    private $domXpathStr;
+
+    /**
+     *
+     * @var SdiNamespaceDao 
+     */
+    private $nsdao;
+
+    function __construct() {
+        $this->session = JFactory::getSession();
+        $this->db = JFactory::getDbo();
+        $this->structure = new DOMDocument('1.0', 'utf-8');
+        $this->structure->loadXML(unserialize($this->session->get('structure')));
+        $this->nsdao = new SdiNamespaceDao();
+
+        parent::__construct();
+    }
+
+    public function removeNode() {
+        $this->domXpathStr = new DOMXPath($this->structure);
+
+        foreach ($this->nsdao->getAll() as $ns) {
+            $this->domXpathStr->registerNamespace($ns->prefix, $ns->uri);
+        }
+        $query = $this->unSerializeXpath($_GET['uuid']);
+        $element = $this->domXpathStr->query($query)->item(0);
+
+        $response = array();
+        try {
+            $element->parentNode->removeChild($element);
+            $response['success'] = 'true';
+
+            $this->session->set('structure', serialize($this->structure->saveXML()));
+        } catch (Exception $exc) {
+            $response['success'] = 'false';
+            $response['message'] = $exc->getMessage();
+        }
+
+        echo json_encode($response);
+        die();
+    }
+
+    public function getBoundaryByCategory() {
+        $user = JFactory::getUser();
+        $default_lang = $user->getParam('language');
+
+        $query = $this->db->getQuery(true);
+        $query->select('t.text1 as option_value, b.`name`, b.northbound, b.southbound, b.westbound, b.eastbound ');
+        $query->from('#__sdi_boundary AS b');
+        $query->innerJoin('#__sdi_boundarycategory as bc ON b.category_id = bc.id');
+        $query->innerJoin('#__sdi_translation t ON b.guid = t.element_guid');
+        $query->innerJoin('jos_sdi_language as l ON l.id = t.language_id');
+        $query->where('bc.`name` = \'' . $_GET['value'] . '\'');
+        $query->where('l.code = \'' . $default_lang . '\'');
+        
+
+        $this->db->setQuery($query);
+        $results = $this->db->loadObjectList();
+
+        echo json_encode($results);
+        die();
+    }
+    
+    public function getBoundaryByName(){
+        $query = $this->db->getQuery(true);
+        $query->select('t.text1, b.northbound, b.southbound, b.westbound, b.eastbound');
+        $query->from('#__sdi_boundary AS b');
+        $query->innerJoin('jos_sdi_translation t ON b.guid = t.element_guid ');
+        $query->where('t.text1 = \'' . $_GET['value'] . '\'');
+        
+        $this->db->setQuery($query);
+        $result = $this->db->loadObject();
+        
+        echo json_encode($result);
+        die();
+    }
+
+    private function unSerializeXpath($xpath) {
+        $xpath = str_replace('-la-', '[', $xpath);
+        $xpath = str_replace('-ra-', ']', $xpath);
+        $xpath = str_replace('-sla-', '/', $xpath);
+        $xpath = str_replace('-dp-', ':', $xpath);
+        return $xpath;
+    }
+
+}
