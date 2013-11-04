2224cc0fb39e2516e6ddbd97ddc49623e9744575
Hélène Van Hoecke <2844416+hvanhoecke@users.noreply.github.com>
2013-11-04 17:54:35 +0000

    BE home

--- publish/java/1.6/sdi_publish/src/main/java/org/easysdi/publish/exception/DatabaseForDiffuserNotfoundException.java
+++
@@ -1,7 +0,0 @@
-package org.easysdi.publish.exception;
-
-public class DatabaseForDiffuserNotfoundException extends Exception {
-	public DatabaseForDiffuserNotfoundException(String s){
-		super(s);
-	}
-}
