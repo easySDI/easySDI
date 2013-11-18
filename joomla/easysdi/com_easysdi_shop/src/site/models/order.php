4a1c3bf7bbc6e70f35f61b8b9e2aaa3095d099f5
Marc Battaglia <4466723+marcbat@users.noreply.github.com>
2013-11-18 07:52:33 +0000

    4.0.0-alpha-19

---
+++ joomla/easysdi/com_easysdi_shop/src/site/models/order.php
@@ -0,0 +1,217 @@
+<?php
+
+/**
+ * @version     4.0.0
+ * @package     com_easysdi_shop
+ * @copyright   Copyright (C) 2013. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      EasySDI Community <contact@easysdi.org> - http://www.easysdi.org
+ */
+// No direct access.
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.modelform');
+jimport('joomla.event.dispatcher');
+
+/**
+ * Easysdi_shop model.
+ */
+class Easysdi_shopModelOrder extends JModelForm {
+
+    var $_item = null;
+
+    /**
+     * Method to auto-populate the model state.
+     *
+     * Note. Calling getState in this method will result in recursion.
+     *
+     * @since	1.6
+     */
+    protected function populateState() {
+        $app = JFactory::getApplication('com_easysdi_shop');
+
+        // Load state from the request userState on edit or from the passed variable on default
+        if (JFactory::getApplication()->input->get('layout') == 'edit') {
+            $id = JFactory::getApplication()->getUserState('com_easysdi_shop.edit.order.id');
+        } else {
+            $id = JFactory::getApplication()->input->get('id');
+            JFactory::getApplication()->setUserState('com_easysdi_shop.edit.order.id', $id);
+        }
+        $this->setState('order.id', $id);
+
+        // Load the parameters.
+        $params = $app->getParams();
+        $params_array = $params->toArray();
+        if (isset($params_array['item_id'])) {
+            $this->setState('order.id', $params_array['item_id']);
+        }
+        $this->setState('params', $params);
+    }
+
+    /**
+     * Method to get an ojbect.
+     *
+     * @param	integer	The id of the object to get.
+     *
+     * @return	mixed	Object on success, false on failure.
+     */
+    public function &getData($id = null) {
+        if ($this->_item === null) {
+            $this->_item = false;
+
+            if (empty($id)) {
+                $id = $this->getState('order.id');
+            }
+            
+            // Get a level row instance.
+            $table = $this->getTable();
+            
+            // Attempt to load the row.
+            if ($table->load($id)) {
+                // Convert the JTable to a clean JObject.
+                $properties = $table->getProperties(1);
+                $this->_item = JArrayHelper::toObject($properties, 'JObject');
+                
+                //Get constante value (to display)
+                $this->_item->orderstate = constant('Easysdi_shopTableorder::orderstate_'.$this->_item->orderstate_id);
+                $this->_item->ordertype = constant('Easysdi_shopTableorder::ordertype_'.$this->_item->ordertype_id);
+                                
+            } elseif ($error = $table->getError()) {
+                $this->setError($error);
+            }
+            
+            $basket = new sdiBasket();
+            $basket->loadOrder($id);
+            
+            $this->_item->basket = $basket;            
+        }
+
+        return $this->_item;
+    }
+
+    public function getTable($type = 'Order', $prefix = 'Easysdi_shopTable', $config = array()) {
+        $this->addTablePath(JPATH_COMPONENT_ADMINISTRATOR . '/tables');
+        return JTable::getInstance($type, $prefix, $config);
+    }
+
+    /**
+     * Method to check in an item.
+     *
+     * @param	integer		The id of the row to check out.
+     * @return	boolean		True on success, false on failure.
+     * @since	1.6
+     */
+    public function checkin($id = null) {
+        // Get the id.
+        $id = (!empty($id)) ? $id : (int) $this->getState('order.id');
+
+        if ($id) {
+
+            // Initialise the table
+            $table = $this->getTable();
+
+            // Attempt to check the row in.
+            if (method_exists($table, 'checkin')) {
+                if (!$table->checkin($id)) {
+                    $this->setError($table->getError());
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Method to check out an item for editing.
+     *
+     * @param	integer		The id of the row to check out.
+     * @return	boolean		True on success, false on failure.
+     * @since	1.6
+     */
+    public function checkout($id = null) {
+        // Get the user id.
+        $id = (!empty($id)) ? $id : (int) $this->getState('order.id');
+
+        if ($id) {
+
+            // Initialise the table
+            $table = $this->getTable();
+
+            // Get the current user object.
+            $user = JFactory::getUser();
+
+            // Attempt to check the row out.
+            if (method_exists($table, 'checkout')) {
+                if (!$table->checkout($user->get('id'), $id)) {
+                    $this->setError($table->getError());
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Method to get the profile form.
+     *
+     * The base form is loaded from XML 
+     * 
+     * @param	array	$data		An optional array of data for the form to interogate.
+     * @param	boolean	$loadData	True if the form is to load its own data (default case), false if not.
+     * @return	JForm	A JForm object on success, false on failure
+     * @since	1.6
+     */
+    public function getForm($data = array(), $loadData = true) {
+        // Get the form.
+        $form = $this->loadForm('com_easysdi_shop.order', 'order', array('control' => 'jform', 'load_data' => $loadData));
+        if (empty($form)) {
+            return false;
+        }
+
+        return $form;
+    }
+
+    /**
+     * Method to get the data that should be injected in the form.
+     *
+     * @return	mixed	The data for the form.
+     * @since	1.6
+     */
+    protected function loadFormData() {
+        $data = $this->getData();
+
+        return $data;
+    }
+
+    
+
+    function delete($data) {
+        $id = (!empty($data['id'])) ? $data['id'] : (int) $this->getState('order.id');
+        if (JFactory::getUser()->authorise('core.delete', 'com_easysdi_shop.order.' . $id) !== true) {
+            JError::raiseError(403, JText::_('JERROR_ALERTNOAUTHOR'));
+            return false;
+        }
+        $table = $this->getTable();
+        if ($table->delete($data['id']) === true) {
+            return $id;
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+    function getCategoryName($id) {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query
+                ->select('title')
+                ->from('#__categories')
+                ->where('id = ' . $id);
+        $db->setQuery($query);
+        return $db->loadObject();
+    }
+
+}
---
+++ joomla/easysdi/plg_easysdi_contact/src/script.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @version     4.0.0
+ * @package     plg_easysdi_user
+ * @copyright   Copyright (C) 2012. All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE.txt
+ * @author      EasySDI Community <contact@easysdi.org> - http://www.easysdi.org
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+class plgUserEasysdicontactInstallerScript
+{
+
+	function preflight( $type, $parent ) {
+		if($type == 'install'){
+			//Check if com_easysdi_core is installed
+			$db = JFactory::getDbo();
+			$db->setQuery('SELECT COUNT(*) FROM #__extensions WHERE name = "com_easysdi_core"');
+			$install = $db->loadResult();
+			if($install == 0){
+				JError::raiseWarning(null, JText::_('PLG_EASYSDIUSER_INSTALL_SCRIPT_CORE_ERROR'));
+				return false;
+			}
+			
+			// Installing component manifest file version
+			$this->release = $parent->get( "manifest" )->version;
+			// Show the essential information at the install/update back-end
+			echo '<p>EasySDI plugin User [plg_user_easysdi]';
+			echo '<br />'.JText::_('Installing plugin manifest file version = ') . $this->release;
+		}
+	}
+ 
+	function install( $parent ) {
+	}
+ 
+	function update( $parent ) {
+	}
+ 
+	function postflight( $type, $parent ) {
+		if($type == 'install'){
+			//Activate the plugin
+			$db = JFactory::getDbo();
+			$db->setQuery("UPDATE #__extensions SET enabled=1 WHERE type='plugin' AND element='easysdicontact' AND folder='user'");
+			$db->execute();
+		}
+	}
+
+	function uninstall( $parent ) {
+	}
+ 
+}
