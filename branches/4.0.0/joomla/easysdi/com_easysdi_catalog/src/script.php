40215dc07c502b5dab529d688f0bc8a095eea390
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2013-11-19 10:28:45 +0000

    4.0.0-alpha-21

--- branches/4.0.0/joomla/easysdi/com_easysdi_catalog/src/script.php
+++ branches/4.0.0/joomla/easysdi/com_easysdi_catalog/src/script.php
@@ -58,7 +58,7 @@ class com_easysdi_catalogInstallerScript {
      */
 
     function postflight($type, $parent) {
-        if ($type == 'install' || ($type == 'update' && ($this->getParam('version') < '4.0.0-alpha-20') == 1 )) {
+        if ($type == 'install' || ($type == 'update' && ($this->getParam('version') < '4.0.0-alpha-22') == 1 )) {
             JTable::addIncludePath(JPATH_ADMINISTRATOR . "/components/com_easysdi_catalog/tables");
             //Create system search criteria
             $sc = JTable::getInstance('searchcriteria', 'easysdi_catalogTable');
