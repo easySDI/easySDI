7f2f52020bb797b6df934e1d0f6e07f673d9ab08
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2013-12-10 14:47:05 +0000

    Mise à jour du modèle de données du map

--- sandboxes/depth/running/hvh/4.0.0/joomla/easysdi/com_easysdi_map/src/administrator/sql/updates/4.0.0-beta-4.sql
+++ sandboxes/depth/running/hvh/4.0.0/joomla/easysdi/com_easysdi_map/src/administrator/sql/updates/4.0.0-beta-4.sql
@@ -15,3 +15,11 @@ VALUES
 ('layerdownload',19,1,'Layer download'),
 ('layerorder',20,1,'Layer order')
 ;
+
+ALTER TABLE `#__sdi_visualization` DROP `wmsservice_id` ;
+ALTER TABLE `#__sdi_visualization` DROP `wmsservicetype_id` ;
+ALTER TABLE `#__sdi_visualization` DROP `layername` ;
+ALTER TABLE `#__sdi_visualization` DROP `attribution` ;
+
+ALTER TABLE `#__sdi_maplayer` DROP `user` ;
+ALTER TABLE `#__sdi_maplayer` DROP `password` ;
