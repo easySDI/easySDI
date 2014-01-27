3367e7672200cf7e86eebaaad1eaa0a69f0278df
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-01-27 15:37:21 +0000

    Correction #8883

--- sandboxes/depth/running/mb/4.0.0-rc-1/joomla/easysdi/com_easysdi_catalog/src/site/libraries/easysdi/FormGenerator.php
+++ sandboxes/depth/running/mb/4.0.0-rc-1/joomla/easysdi/com_easysdi_catalog/src/site/libraries/easysdi/FormGenerator.php
@@ -271,8 +271,8 @@ class FormGenerator {
                     $relation = $this->getDomElement($result->uri, $result->prefix, $result->name, $result->id, EnumChildtype::$RELATION, $result->guid, $result->lowerbound, $result->upperbound);
                     $class = $this->getDomElement($result->class_ns_uri, $result->class_ns_prefix, $result->class_name, $result->class_id, EnumChildtype::$CLASS, $result->class_guid, null, null, $result->class_stereotype_id);
 
-                    $relation->setAttributeNS($this->catalog_uri, $this->catalog_prefix.':level', $level);
-                    
+                    $relation->setAttributeNS($this->catalog_uri, $this->catalog_prefix . ':level', $level);
+
                     switch ($result->class_stereotype_id) {
                         case EnumStereotype::$GEOGRAPHICEXTENT:
                             $params = array();
@@ -1227,6 +1227,7 @@ class FormGenerator {
      */
     private function getFormHiddenField(DOMElement $attribute) {
         $relid = $attribute->getAttributeNS($this->catalog_uri, 'relid');
+        $stereotypeid = $attribute->getAttributeNS($this->catalog_uri, 'stereotypeId');
 
         $attributename = $attribute->nodeName;
         $field = $this->form->createElement('field');
@@ -1234,7 +1235,18 @@ class FormGenerator {
 
         $field->setAttribute('name', FormUtils::serializeXpath($attribute->firstChild->getNodePath()));
         $field->setAttribute('type', 'hidden');
-        $field->setAttribute('default', $this->getDefaultValue($relid, $attribute->firstChild->nodeValue));
+
+        switch ($stereotypeid) {
+            case 6:
+            case 9:
+                $field->setAttribute('default', $this->getDefaultValue($relid, $attribute->firstChild->nodeValue, true));
+                break;
+
+            default:
+                $field->setAttribute('default', $this->getDefaultValue($relid, $attribute->firstChild->nodeValue));
+                break;
+        }
+
         $field->setAttribute('label', 'label');
 
         return $field;
