f1b06d5a7317ff1ec40d7d7efa3a8d09c6cfb899
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-01-31 13:21:41 +0000

    Branch depuis la revision 6740 de la branch 4.0.0-rc-1

---
+++ sandboxes/depth/running/mb/4.0.0-rc-1-merge/com_easysdi_core/src/site/views/version/tmpl/version.js
@@ -0,0 +1,103 @@
+js = jQuery.noConflict();
+var childrenTable, availablechildrenTable, parents;
+js(document).ready(function() {
+
+    availablechildrenTable = js('#sdi-availablechildren').dataTable({
+        "bFilter": false,
+        "bLengthChange": false,
+        "aoColumnDefs": [
+            {"bVisible": false, "aTargets": [0]},
+            {"bVisible": versioning, "aTargets": [2]}
+        ],
+        "oLanguage": {
+            "sSearch": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SEARCH'),
+            "sZeroRecords": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_NORESULT'),
+            "sInfo": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SHOWING') + " _START_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_TO') + " _END_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_OF') + " _TOTAL_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_RECORDS'),
+            "sInfoEmpty": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SHOWING') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_TO') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_OF') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_RECORDS'),
+            "sInfoFiltered": "(filtered from _MAX_ total records)"
+
+                    ,
+            "oPaginate": {
+                "sNext": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_NEXT'),
+                "sPrevious": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_PREVIOUS')
+            }
+        }
+    });
+
+    childrenTable = js('#sdi-children').dataTable({
+        "bLengthChange": false,
+        "aoColumnDefs": [
+            {"bVisible": false, "aTargets": [0]},
+            {"bVisible": versioning, "aTargets": [2]},
+            {"sClass": "center", "aTargets": [5]}
+        ],
+        "oLanguage": {
+            "sSearch": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SEARCH'),
+            "sZeroRecords": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_NORESULT'),
+            "sInfo": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SHOWING') + " _START_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_TO') + " _END_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_OF') + " _TOTAL_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_RECORDS'),
+            "sInfoEmpty": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SHOWING') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_TO') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_OF') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_RECORDS'),
+            "sInfoFiltered": "(filtered from _MAX_ total records)"
+
+                    ,
+            "oPaginate": {
+                "sNext": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_NEXT'),
+                "sPrevious": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_PREVIOUS')
+            }
+        }
+    });
+    parents = js('#sdi-parents').dataTable({
+        "bFilter": true,
+        "bLengthChange": false,
+        "aoColumnDefs": [
+            {"bVisible": false, "aTargets": [0]},
+            {"bVisible": versioning, "aTargets": [2]}
+        ],
+        "oLanguage": {
+            "sSearch": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SEARCH'),
+            "sZeroRecords": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_NORESULT'),
+            "sInfo": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SHOWING') + " _START_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_TO') + " _END_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_OF') + " _TOTAL_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_RECORDS'),
+            "sInfoEmpty": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SHOWING') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_TO') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_OF') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_RECORDS'),
+            "sInfoFiltered": "(filtered from _MAX_ total records)"
+
+                    ,
+            "oPaginate": {
+                "sNext": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_NEXT'),
+                "sPrevious": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_PREVIOUS')
+            }
+        }
+    });
+
+
+});
+
+function addChild(child) {
+    js('#sdi-children').dataTable().fnAddData([
+        child.id,
+        child.resource,
+        child.version,
+        child.resourcetype,
+        Joomla.JText._(child.state),
+        '<button type="button" id="sdi-childbutton-' + child.id + '" onClick="deleteChild(\'' + child.id + '\');" class="btn btn-warning btn-mini"><i class="icon-white icon-minus"></i></button>'
+
+    ]);
+}
+
+function deleteChild(child) {
+    childrenTable.fnDeleteRow(js('#sdi-childbutton-' + child).parent().parent()[0]);
+}
+
+Joomla.submitbutton = function(task)
+{
+    if (task === 'version.save' || task === 'version.apply') {
+        var results = [];
+        var children = childrenTable.fnGetData();
+        children.each(function(value) {
+            results.push(value[0]);
+        });
+
+        var r = JSON.stringify(results);
+
+        js('#jform_selectedchildren').val(r);
+    }
+    Joomla.submitform(task, document.getElementById('adminForm'));
+}
---
+++ sandboxes/depth/running/mb/4.0.0-rc-1-merge/com_easysdi_service/src/administrator/helpers/PhysicalService.php
@@ -0,0 +1,82 @@
+<?php
+
+abstract class PhysicalService {
+
+    public $id;
+    public $name;
+    public $url;
+    protected $rawXml;
+    protected $compliance;
+    protected $connector;
+    protected $xmlCapabilities;
+    protected $user;
+    protected $password;
+
+    abstract public function populate();
+
+    abstract public function sortLists();
+
+    abstract public function loadData($data);
+
+    abstract public function setLayerAsConfigured($layerList);
+
+    public function __construct($id, $url, $user, $password, $connector) {
+        $this->id = $id;
+        $this->url = $url;
+        $this->connector = $connector;
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    public function getRawXml() {
+        return $this->rawXml;
+    }
+
+    /**
+     * Request the Capabilities of the server and store them.
+     * 
+     * @param string $rawXML : optional xmlString that contains the capabilities of the server (no request is made if that param is set)
+     * 
+     * @return bool : true on success, false on error
+     */
+    public function getCapabilities($rawXML = null) {
+        $this->rawXml = $rawXML;
+        if (!isset($rawXML)) {
+            $completeUrl = $this->url . "?REQUEST=GetCapabilities&SERVICE=" . $this->connector;
+            if (isset($this->compliance)) {
+                $completeUrl .= "&version=" . $this->compliance;
+            }
+
+            $session = curl_init($completeUrl);
+            if (!empty($this->user) && !empty($this->password)) {
+                curl_setopt($session, CURLOPT_HTTPHEADER, Array('Authorization: Basic ' . base64_encode($this->user . ':' . $this->password)));
+            }
+            curl_setopt($session, CURLOPT_HEADER, false);
+            curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+            $this->rawXml = curl_exec($session);
+            $http_status = curl_getinfo($session, CURLINFO_HTTP_CODE);
+            curl_close($session);
+
+            //HTTP status error
+            if ($http_status != '200') {
+//				echo $completeUrl;
+                return false;
+            }
+        }
+
+        $xmlCapa = simplexml_load_string($this->rawXml);
+
+        $namespaces = $xmlCapa->getNamespaces(true);
+        foreach ($namespaces as $key => $value) {
+            if ($key == '') {
+                $xmlCapa->registerXPathNamespace("dflt", $value);
+            } else {
+                $xmlCapa->registerXPathNamespace($key, $value);
+            }
+        }
+
+        $this->xmlCapabilities = $xmlCapa;
+        return true;
+    }
+
+}
