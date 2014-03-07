d94cec0b369acc421a9421c770eaba38013ce67b
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-03-07 10:10:32 +0000

    4.0.0-rc-5

---
+++ tags/4.0.0-rc-5/java/easysdi/proxy/src/main/java/org/easysdi/proxy/domain/SdiPhysicalservicePolicyHome.java
@@ -0,0 +1,105 @@
+package org.easysdi.proxy.domain;
+
+// Generated Apr 9, 2013 11:54:42 AM by Hibernate Tools 3.4.0.CR1
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Home object for domain model class SdiPhysicalservicePolicy.
+ * @see org.easysdi.proxy.domain.SdiPhysicalservicePolicy
+ * @author Hibernate Tools
+ */
+
+@Transactional
+@Repository
+public class SdiPhysicalservicePolicyHome {
+
+	private static final Log log = LogFactory
+			.getLog(SdiPhysicalservicePolicyHome.class);
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public SdiPhysicalservicePolicy findById(Integer id) {
+		log.debug("getting SdiPhysicalservicePolicy instance with id: " + id);
+		try {
+			SdiPhysicalservicePolicy instance = (SdiPhysicalservicePolicy) sessionFactory
+					.getCurrentSession()
+					.get(SdiPhysicalservicePolicy.class, id);
+			log.debug("get successful");
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public void save(SdiPhysicalservicePolicy transientInstance) {
+		log.debug("save SdiPhysicalservicePolicy instance");
+		try {
+			sessionFactory.getCurrentSession().save(transientInstance);
+			log.debug("save successful");
+		} catch (RuntimeException re) {
+			log.error("save failed", re);
+			throw re;
+		}
+	}
+
+	public void saveOrUpdate(SdiPhysicalservicePolicy transientInstance) {
+		log.debug("saveOrUpdate SdiPhysicalservicePolicy instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(transientInstance);
+			log.debug("saveOrUpdate successful");
+		} catch (RuntimeException re) {
+			log.error("saveOrUpdate failed", re);
+			throw re;
+		}
+	}
+
+	public void update(SdiPhysicalservicePolicy transientInstance) {
+		log.debug("update SdiPhysicalservicePolicy instance");
+		try {
+			sessionFactory.getCurrentSession().update(transientInstance);
+			log.debug("update successful");
+		} catch (RuntimeException re) {
+			log.error("update failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(SdiPhysicalservicePolicy transientInstance) {
+		log.debug("delete SdiPhysicalservicePolicy instance");
+		try {
+			sessionFactory.getCurrentSession().delete(transientInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public void merge(SdiPhysicalservicePolicy transientInstance) {
+		log.debug("merge SdiPhysicalservicePolicy instance");
+		try {
+			sessionFactory.getCurrentSession().merge(transientInstance);
+			log.debug("merge successful");
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+}
---
+++ tags/4.0.0-rc-5/joomla/easysdi/com_easysdi_shop/src/site/views/basket/tmpl/freeperimeter.js
@@ -0,0 +1,93 @@
+function initDraw() {
+    polygonLayer = new OpenLayers.Layer.Vector("Polygon Layer", {srsName: app.mapPanel.map.projection, projection: app.mapPanel.map.projection});
+    polygonLayer.events.on({
+        featuresadded: onFeaturesAdded,
+        beforefeatureadded: beforeFeatureAdded
+    });
+    
+    polygonLayer.events.register("featureadded", polygonLayer, listenerFeatureAdded);
+    app.mapPanel.map.addLayers([polygonLayer]);
+
+    drawControls = {
+        polygon: new OpenLayers.Control.DrawFeature(polygonLayer, OpenLayers.Handler.Polygon, {handlerOptions: {stopDown: 0, stopUp: 0}}),
+        box: new OpenLayers.Control.DrawFeature(polygonLayer, OpenLayers.Handler.RegularPolygon, {handlerOptions: {stopDown: 1, stopUp: 1, irregular: 1}})
+    };
+    for (var key in drawControls) {
+        app.mapPanel.map.addControl(drawControls[key]);
+    }
+}
+
+function onFeaturesAdded(event) {
+    putFeaturesVerticesInHiddenField(event.features[0].clone());
+}
+
+function putFeaturesVerticesInHiddenField(feature) {
+    jQuery('#t-surface').val(JSON.stringify(feature.geometry.getGeodesicArea(app.mapPanel.map.projection)));
+    
+    var geometry = feature.geometry.transform(
+            new OpenLayers.Projection(app.mapPanel.map.projection),
+            new OpenLayers.Projection("EPSG:4326")
+            );
+
+    var wkt = new OpenLayers.Format.WKT();
+    var featureAsString = wkt.write(feature);
+
+    jQuery('#t-features').val(JSON.stringify(featureAsString));
+}
+
+function selectPerimeter1() {
+    selectRectangle();
+}
+
+function reloadFeatures1() {
+    var wkt = jQuery('#features').val();
+    var feature = new OpenLayers.Format.WKT().read(wkt);
+    var geometry = feature.geometry.transform(
+            new OpenLayers.Projection("EPSG:4326"),
+            new OpenLayers.Projection(app.mapPanel.map.projection)
+            );
+    polygonLayer.addFeatures([feature]);
+    app.mapPanel.map.zoomToExtent(polygonLayer.getDataExtent());
+ /*   app.mapPanel.map.zoomToExtent(geometry.getBounds().transform(
+            new OpenLayers.Projection("EPSG:4326"),
+            new OpenLayers.Projection(app.mapPanel.map.projection)
+            ));*/
+    putFeaturesVerticesInHiddenField(feature.clone());
+}
+
+var listenerFeatureDrawToZoom = function(e) {
+    app.mapPanel.map.zoomToExtent(polygonLayer.getDataExtent());
+};
+
+function selectPolygon() {
+    resetAll();
+    selectControl = new OpenLayers.Control.DrawFeature(polygonLayer, OpenLayers.Handler.Polygon, {handlerOptions: {stopDown: 0, stopUp: 0}});
+    
+    app.mapPanel.map.addControl(selectControl);
+    jQuery('#t-perimeter').val('1');
+    jQuery('#t-perimetern').val(Joomla.JText._('FREEPERIMETER', 'Périmètre libre'));
+    jQuery('#t-features').val('');
+    toggleSelectControl('selection');
+}
+
+function selectRectangle() {
+    resetAll();
+    selectControl = new OpenLayers.Control.DrawFeature(polygonLayer, OpenLayers.Handler.RegularPolygon, {handlerOptions: {stopDown: 1, stopUp: 1, irregular: 1}});
+    
+    app.mapPanel.map.addControl(selectControl);
+    jQuery('#t-perimeter').val('1');
+    jQuery('#t-perimetern').val(Joomla.JText._('FREEPERIMETER', 'Périmètre libre'));
+    jQuery('#t-features').val('');
+    toggleSelectControl('selection');
+}
+
+function toggleRectangle() {
+   selectControl = new OpenLayers.Control.DrawFeature(polygonLayer, OpenLayers.Handler.RegularPolygon, {handlerOptions: {stopDown: 1, stopUp: 1, irregular: 1}});
+    
+    app.mapPanel.map.addControl(selectControl);
+    jQuery('#t-perimeter').val('1');
+    jQuery('#t-perimetern').val(Joomla.JText._('FREEPERIMETER', 'Périmètre libre'));
+    jQuery('#t-features').val('');
+    toggleSelectControl('selection');
+}
+
