ba7cf73aa2f4f2c796840e029a4702da4ce67275
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-03-24 08:13:31 +0000

    Branch pour version stable 4.0.0

---
+++ sandboxes/depth/running/mb/4.0.0/easysdi/joomla/easysdi/com_easysdi_catalog/src/administrator/sql/updates/4.0.0-beta-6.sql
@@ -0,0 +1,1 @@
+ALTER TABLE #__sdi_attribute DROP COLUMN issystem;
---
+++ sandboxes/depth/running/mb/4.0.0/easysdi/joomla/easysdi/com_easysdi_contact/src/administrator/sql/updates/4.0.0-beta-6.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `jos_sdi_organism` ADD `username` VARCHAR(150);
+ALTER TABLE `jos_sdi_organism` ADD `password` VARCHAR(65);
