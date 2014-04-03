b30664d0437192f26d219560671e8422359be041
tportier <tportier@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-04-03 06:32:06 +0000

    install crash + correction css + jos_ on code

--- sandboxes/depth.fr/tp/4.0.1/com_easysdi_core/src/site/views/resources/tmpl/default.php
+++ sandboxes/depth.fr/tp/4.0.1/com_easysdi_core/src/site/views/resources/tmpl/default.php
@@ -20,40 +20,7 @@ JHtml::_('behavior.calendar');
 $document = JFactory::getDocument();
 $document->addScript('administrator/components/com_easysdi_core/libraries/easysdi/catalog/resources.js');
 ?>
-<style>
-    div.modal {
-	position: fixed;
-	top: 50%;
-	left: 50%;
-	z-index: 1050;
-	overflow: auto;
-	width: 560px;
-	margin: -250px 0 0 -280px;
-	background-color: #fff;
-	border: 1px solid #999;
-	border: 1px solid rgba(0,0,0,0.3);
-	*border: 1px solid #999;
-	-webkit-border-radius: 6px;
-	-moz-border-radius: 6px;
-	border-radius: 6px;
-	-webkit-box-shadow: 0 3px 7px rgba(0,0,0,0.3);
-	-moz-box-shadow: 0 3px 7px rgba(0,0,0,0.3);
-	box-shadow: 0 3px 7px rgba(0,0,0,0.3);
-	-webkit-background-clip: padding-box;
-	-moz-background-clip: padding-box;
-	background-clip: padding-box;
-}
-div.modal.fade {
-	-webkit-transition: opacity .3s linear;
-	-moz-transition: opacity .3s linear;
-	-o-transition: opacity .3s linear;
-	transition: opacity .3s linear;
-	top: -25%;
-}
-div.modal.fade.in {
-	top: 50%;
-}
-</style>
+
 <div class="core front-end-edit">
     <h1><?php echo JText::_('COM_EASYSDI_CORE_TITLE_RESOURCES'); ?></h1>
     <?php
