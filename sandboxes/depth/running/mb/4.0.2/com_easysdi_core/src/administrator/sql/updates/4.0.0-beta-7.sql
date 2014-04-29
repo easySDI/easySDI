42bccf4abd4f8df72421a25faa5780baad2e1126
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-04-29 14:46:13 +0000

    4.0.2

---
+++ sandboxes/depth/running/mb/4.0.2/com_easysdi_core/src/administrator/sql/updates/4.0.0-beta-7.sql
@@ -0,0 +1,1 @@
+INSERT INTO `#__sdi_sys_productstate` VALUES ('3', '1', '1', 'sent');
---
+++ sandboxes/depth/running/mb/4.0.2/com_easysdi_shop/src/administrator/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,16 @@
+SET foreign_key_checks = 0;
+
+DROP TABLE IF EXISTS `#__sdi_diffusion`;
+DROP TABLE IF EXISTS `#__sdi_property`;
+DROP TABLE IF EXISTS `#__sdi_propertyvalue`;
+DROP TABLE IF EXISTS `#__sdi_diffusion_perimeter`;
+DROP TABLE IF EXISTS `#__sdi_diffusion_propertyvalue`;
+DROP TABLE IF EXISTS `#__sdi_diffusion_notifieduser`;
+DROP TABLE IF EXISTS `#__sdi_perimeter`;
+DROP TABLE IF EXISTS `#__sdi_grid`;
+DROP TABLE IF EXISTS `#__sdi_order`;
+DROP TABLE IF EXISTS `#__sdi_order_diffusion`;
+DROP TABLE IF EXISTS `#__sdi_order_propertyvalue`;
+DROP TABLE IF EXISTS `#__sdi_order_perimeter`;
+
+SET foreign_key_checks = 1;
