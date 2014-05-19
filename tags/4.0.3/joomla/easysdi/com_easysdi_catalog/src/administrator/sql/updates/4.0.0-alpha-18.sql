acdc7f7ca0f02f1549b41e0c70f46a37be761bca
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-05-19 07:43:36 +0000

    4.0.3

---
+++ tags/4.0.3/joomla/easysdi/com_easysdi_catalog/src/administrator/sql/updates/4.0.0-alpha-18.sql
@@ -0,0 +1,4 @@
+INSERT INTO `#__sdi_sys_maptool` (alias,ordering,state,name) 
+VALUES 
+('searchcatalog',17,1,'Catalog search')
+;
---
+++ tags/4.0.3/joomla/easysdi/com_easysdi_service/src/administrator/tables/tilematrixset.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @version     3.3.0
+ * @package     com_easysdi_service
+ * @copyright   Copyright (C) 2013. All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE.txt
+ * @author      EasySDI Community <contact@easysdi.org> - http://www.easysdi.org
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+require_once JPATH_ADMINISTRATOR.DS.'components'.DS.'com_easysdi_core'.DS.'libraries'.DS.'easysdi'.DS.'database'.DS.'sditable.php';
+/**
+ * virtualmetadata Table class
+ */
+class Easysdi_serviceTabletilematrixset extends sdiTable {
+
+	/**
+	* Constructor
+	*
+	* @param JDatabase A database connector object
+	*/
+	public function __construct(&$db) {
+		parent::__construct('#__sdi_tilematrixset', 'id', $db);
+	}
+	
+	public function save($src, $orderingFilter = '', $ignore = '') {
+		$data = array();
+		//$data['guid'] 							= $src['guid'];
+		$data['identifier'] 			= $src['identifier'];
+		$data['supported_crs'] 		= $src['supported_crs'];
+		$data['wmtslayer_id'] 		= $src['wmtslayer_id'];
+		return parent::save($data, $orderingFilter , $ignore );
+	}
+	
+	public function getListByWMTSLayer($wmtsLayerID) {
+		$db = JFactory::getDbo();
+		$db->setQuery('
+			SELECT *
+			FROM #__sdi_tilematrixset
+			WHERE wmtslayer_id = ' . $wmtsLayerID . ';
+		');
+		
+		try {
+			$resultSet = $db->loadObjectList();
+		}
+		catch (JDatabaseException $e) {
+			$je = new JException($e->getMessage());
+			$this->setError($je);
+			return false;
+		}
+
+		// Legacy error handling switch based on the JError::$legacy switch.
+		// @deprecated  12.1
+		if (JError::$legacy && $this->_db->getErrorNum())	{
+			$e = new JException($this->_db->getErrorMsg());
+			$this->setError($e);
+			return false;
+		}
+		
+		return $resultSet;
+	}
+}
