7fcc08a74e595efa7427b82cd4468b45c149c790
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-06-03 15:20:01 +0000

    Backup non stable #8846

--- sandboxes/depth/running/mb/4.2.0/joomla/easysdi/com_easysdi_core/src/site/controllers/version.php
+++ sandboxes/depth/running/mb/4.2.0/joomla/easysdi/com_easysdi_core/src/site/controllers/version.php
@@ -20,6 +20,8 @@ require_once JPATH_ADMINISTRATOR . '/components/com_easysdi_core/helpers/easysdi
  */
 class Easysdi_coreControllerVersion extends Easysdi_coreController {
 
+    private $versions = array();
+
     public function remove() {
         // Initialise variables.
         $model = $this->getModel('Version', 'Easysdi_coreModel');
@@ -27,19 +29,25 @@ class Easysdi_coreControllerVersion extends Easysdi_coreController {
         // Get the user data.
         $data = array();
         $data['id'] = JFactory::getApplication()->input->get('id', null, 'int');
-
+        
+        $version = $model->getData($data['id']);
+        
+        $versions = $this->getViralVersionnedChild($version);
+        
         //First Delete the metadata
         $metadata = JTable::getInstance('metadata', 'Easysdi_catalogTable');
         $keys = array("version_id" => $data['id']);
+        
         $metadata->load($keys);
         //Delete the csw metadata in the remote catalog
         $csw = new sdiMetadata($metadata->id);
-        if (!$csw->delete()):
+        if (!$csw->delete()){
             // Redirect back to the list screen.
             $this->setMessage(JText::_('Metadata can not be deleted from the remote catalog.'), 'error');
 //            $this->setRedirect(JRoute::_('index.php?option=com_easysdi_core&view=resources', false));
 //            return false;
-        endif;
+        }
+        
         if (!$metadata->delete($metadata->id)) {
             // Redirect back to the list screen.
             $this->setMessage(JText::_('Metadata can not be deleted.'), 'error');
@@ -212,41 +220,40 @@ class Easysdi_coreControllerVersion extends Easysdi_coreController {
      * Move it if needed when implementing version surpport.
      */
     function create() {
-        $version = array();
-        $version['resource_id'] = JFactory::getApplication()->input->get('resource', null, 'int');
-        $version['name'] = date("Y-m-d H:i:s");
 
-        $model = $this->getModel('Version', 'Easysdi_coreModel');
-        // Attempt to save the data.
-        $return = $model->save($version);
+        $resource_id = JFactory::getApplication()->input->get('resource', null, 'int');
 
-        // Check for errors.
-        if ($return === false) {
-            // Redirect back to the resources list.
-            $this->setMessage(JText::sprintf('Save failed', $model->getError()), 'error');
-            $this->setRedirect(JRoute::_('index.php?option=com_easysdi_core&view=resources', false));
-            return false;
+        $versions = array();
+        if ($lastversion = $this->getLastVersion($resource_id)) {
+            $versions = $this->getViralVersionnedChild($lastversion);
         }
-
+        
+        $new_versions = $this->getNewVersions($versions);
+        
+        $this->saveVersions($new_versions);
+        
         //Create the linked metadata
         require_once JPATH_SITE . '/components/com_easysdi_catalog/models/metadata.php';
         $metadata = JModelLegacy::getInstance('metadata', 'Easysdi_catalogModel');
-        $mddata = array("metadatastate_id" => 1, "accessscope_id" => 1, "version_id" => $return);
-        if ($metadata->save($mddata) === false) {
-            //Saving metadata in database or metadata in CSW catalog failed
-            //Version must be deleted
-            if (!$model->delete(array('id' => $return))) {
-                //Can not delete version, it's a mess in the database from now...
-                JFactory::getApplication()->enqueueMessage(JText::_('COM_EASYSDI_CORE_RESOURCES_ITEM_SAVED_ERROR_ROLLBACK_VERSION_ERROR'), 'error');
+        $model = $this->getModel('Version', 'Easysdi_coreModel');
+        foreach ($this->versions as $version) {
+            $mddata = array("metadatastate_id" => 1, "accessscope_id" => 1, "version_id" => $version['id']);
+            if ($metadata->save($mddata) === false) {
+                //Saving metadata in database or metadata in CSW catalog failed
+                //Version must be deleted
+                if (!$model->delete(array('id' => $version['id']))) {
+                    //Can not delete version, it's a mess in the database from now...
+                    JFactory::getApplication()->enqueueMessage(JText::_('COM_EASYSDI_CORE_RESOURCES_ITEM_SAVED_ERROR_ROLLBACK_VERSION_ERROR'), 'error');
+                    return false;
+                }
+                JFactory::getApplication()->enqueueMessage(JText::_('COM_EASYSDI_CORE_METADATA_ITEM_SAVED_ERROR'), 'error');
                 return false;
             }
-            JFactory::getApplication()->enqueueMessage(JText::_('COM_EASYSDI_CORE_METADATA_ITEM_SAVED_ERROR'), 'error');
-            return false;
-        }
 
-        // Check in the version.
-        if ($return) {
-            $model->checkin($return);
+            // Check in the version.
+            
+            $model->checkin($version['id']);
+            
         }
 
         // Redirect to the list screen.
@@ -361,6 +368,146 @@ class Easysdi_coreControllerVersion extends Easysdi_coreController {
         $app->setUserState('com_easysdi_core.edit.version.metadataid', null);
         $app->setUserState('com_easysdi_core.edit.version.data', null);
         $app->setUserState('com_easysdi_core.edit.version.runsearch', null);
+    }
+
+    /**
+     * 
+     * @param array $versions
+     */
+    private function getNewVersions($versions) {
+        $new_versions = array();
+
+        foreach ($versions as $version) {
+            $new_version = array();
+            $new_version['resource_id'] = $version->resource_id;
+            $new_version['name'] = date("Y-m-d H:i:s");
+            
+            if(!empty($version->children)){
+                $new_version['children'] = $this->getNewVersions($version->children);
+            }
+            
+            $new_versions[] = $new_version;
+        }
+        
+        return $new_versions;
+    }
+
+    private function deleteVersions($versions){
+        
+    }
+    
+    /**
+     * 
+     * @param array $versions
+     */
+    private function saveVersions($versions) {
+        $model = $this->getModel('Version', 'Easysdi_coreModel');
+        
+        $selected_children = array();
+        
+        foreach ($versions as $version) {
+            if(array_key_exists('children', $version)){
+                $version['selectedchildren'] = $this->saveVersions($version['children']);
+            }
+            
+            // Attempt to save the data.
+            $return = $model->save($version);
+            // Check for errors.
+            if ($return === false) {
+                // Redirect back to the resources list.
+                $this->setMessage(JText::sprintf('Save failed', $model->getError()), 'error');
+                $this->setRedirect(JRoute::_('index.php?option=com_easysdi_core&view=resources', false));
+                $this->rollback($this->versions);
+                return false;
+            } else {
+                $version['id'] = $return;
+                $this->versions[] = $version;
+                $selected_children[] = $version['id'];
+            }
+        }
+        
+        return json_encode($selected_children);
+    }
+
+    /**
+     * Check recursively if version has viral versionning child
+     * 
+     * @param stdClass $version
+     * @return stdClass[] if has child or false
+     */
+    private function getViralVersionnedChild($version) {
+        $all_versions = array();
+
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select('cv.id, cv.name AS version_name, cr.name AS resource_name, cr.id AS resource_id');
+        $query->from('#__sdi_versionlink vl');
+        $query->innerJoin('#__sdi_version pv ON vl.parent_id = pv.id');
+        $query->innerJoin('#__sdi_resource pr ON pv.resource_id = pr.id');
+        $query->innerJoin('#__sdi_resourcetypelink rtl ON pr.resourcetype_id = rtl.parent_id');
+        $query->innerJoin('#__sdi_version cv ON vl.child_id = cv.id');
+        $query->innerJoin('#__sdi_resource cr ON cv.resource_id = cr.id AND cr.resourcetype_id = rtl.child_id');
+        $query->where('rtl.viralversioning = 1');
+        $query->where('vl.parent_id = ' . (int) $version->id);
+
+        $db->setQuery($query);
+        $childs = $db->loadObjectList('id');
+
+        if (count($childs) > 0) {
+            $version->children = $childs;
+            $all_versions[$version->id] = $version;
+            $all_versions[$version->id]->children = $childs;
+
+            foreach ($childs as $key => $child) {
+                $this->getViralVersionnedChild($child);
+            }
+        }
+
+        return $all_versions;
+    }
+
+    /**
+     * Get last resource version
+     * 
+     * @param int $resource_id
+     * @return stdClass or false  The last version
+     */
+    private function getLastVersion($resource_id) {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select('v.id, v.resource_id');
+        $query->from('#__sdi_version v');
+        $query->where('v.resource_id = ' . (int) $resource_id);
+        $query->order('v.id DESC');
+
+        $db->setQuery($query);
+        $version = $db->loadObject();
+
+        if (!empty($version)) {
+            return $version;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Cleans versions already created during the failure of creating a version.
+     * 
+     * @param stdClass[] $versions Version to clean
+     */
+    private function rollback($versions) {
+        $db = JFactory::getDbo();
+
+        foreach ($versions as $version) {
+            $query = $db->getQuery(true);
+            $query->delete('#__version');
+            $query->where('id=' . $version['id']);
+
+            $db->setQuery($query);
+            if (!$db->execute()) {
+                return false;
+            }
+        }
     }
 
 }
