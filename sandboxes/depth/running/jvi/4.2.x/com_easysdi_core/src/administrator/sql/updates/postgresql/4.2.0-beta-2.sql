4d2e5e5c88f1b04fa3c93272b7b2f31b6f4a9064
jvillemagne <jvillemagne@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-06-23 14:20:34 +0000

    [4.2.0-beta-2 - jvi]: - fix bugs : #10109, #10111

--- sandboxes/depth/running/jvi/4.2.x/com_easysdi_core/src/administrator/sql/updates/postgresql/4.2.0-beta-2.sql
+++ sandboxes/depth/running/jvi/4.2.x/com_easysdi_core/src/administrator/sql/updates/postgresql/4.2.0-beta-2.sql
@@ -1,5 +1,5 @@
 
 
-UPDATE `jos_sdi_sys_accessscope` SET ordering=2 WHERE `value`='category'
-UPDATE `jos_sdi_sys_accessscope` SET ordering=3 WHERE `value`='organism'
-UPDATE `jos_sdi_sys_accessscope` SET ordering=4 WHERE `value`='user'
+UPDATE `#__sdi_sys_accessscope` SET ordering=2 WHERE `value`='category'
+UPDATE `#__sdi_sys_accessscope` SET ordering=3 WHERE `value`='organism'
+UPDATE `#__sdi_sys_accessscope` SET ordering=4 WHERE `value`='user'
