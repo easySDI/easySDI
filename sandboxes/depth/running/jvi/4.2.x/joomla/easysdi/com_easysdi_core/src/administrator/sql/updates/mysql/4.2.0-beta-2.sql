f13d9452aa409cba9b58f2c82c309accfaa89210
jvillemagne <jvillemagne@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-06-30 08:23:49 +0000

    Update from 4.2.x

--- sandboxes/depth/running/jvi/4.2.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.2.0-beta-2.sql
+++ sandboxes/depth/running/jvi/4.2.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.2.0-beta-2.sql
@@ -1,5 +1,5 @@
 
 
-UPDATE `#__sdi_sys_accessscope` SET ordering=2 WHERE `value`='category'
-UPDATE `#__sdi_sys_accessscope` SET ordering=3 WHERE `value`='organism'
-UPDATE `#__sdi_sys_accessscope` SET ordering=4 WHERE `value`='user'
+UPDATE `#__sdi_sys_accessscope` SET ordering=2 WHERE `value`='category';
+UPDATE `#__sdi_sys_accessscope` SET ordering=3 WHERE `value`='organism';
+UPDATE `#__sdi_sys_accessscope` SET ordering=4 WHERE `value`='user';
