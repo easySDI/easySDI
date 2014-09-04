1601654b33d667049c26be5b9af70eca6b0f602c
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-09-04 14:26:38 +0000

    delete

--- sandboxes/depth/running/mb/4.2.x-rollback-7199/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.2.0-beta-3.sql
+++
@@ -1,8 +0,0 @@
-
-ALTER TABLE [#__sdi_accessscope] ADD [category_id] [bigint] NULL;
-
-ALTER TABLE [#__sdi_accessscope]  WITH CHECK ADD  CONSTRAINT [#__sdi_accessscope$#__sdi_accessscope_fk3] FOREIGN KEY([category_id])
-REFERENCES [#__sdi_category] ([id])
-ON DELETE CASCADE;
-
-ALTER TABLE [#__sdi_accessscope] CHECK CONSTRAINT [#__sdi_accessscope$#__sdi_accessscope_fk3];
