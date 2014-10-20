11352041ed55232017b1eb44e06d664237aa3f8a
Jérôme VILLEMAGNE <21275566+jvillemagne@users.noreply.github.com>
2014-10-20 07:04:34 +0000

    [jvi]: - fix the synchronization functionnality - fix assignment history according to previous changment + mysql update script

--- joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.2.0-rc-1.sql
+++ joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.2.0-rc-1.sql
@@ -9,10 +9,9 @@ ALTER TABLE `#__sdi_resourcetypelink` DROP FOREIGN KEY `#__sdi_resourcetypelink_
 ALTER TABLE `#__sdi_resourcetypelink` DROP COLUMN `attribute_id`;
 
 
+ALTER TABLE `#__sdi_assignment` DROP FOREIGN KEY `#__sdi_assignment_fk3`;
 ALTER TABLE `#__sdi_assignment` CHANGE `version_id` `metadata_id` INT(11) UNSIGNED NOT NULL;
 TRUNCATE `#__sdi_assignment`;
-
-ALTER TABLE `#__sdi_assignment` DROP FOREIGN KEY `#__sdi_assignment_fk3`;
 ALTER TABLE `#__sdi_assignment` ADD CONSTRAINT `#__sdi_assignment_fk3`
     FOREIGN KEY (`metadata_id` )
     REFERENCES `#__sdi_metadata` (`id` )
