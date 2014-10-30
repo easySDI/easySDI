c354fdb3f65f1694ab9c45ccaee8073ae2ebf724
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-10-30 08:16:48 +0000

    Merged revision(s) 7495-7502 from sandboxes/depth/running/hvh/4.2.x: [jvi]: - fix ESRI import ........ Merged revision(s) 7496-7498 from branches/4.2.x: Auto Commit from continuous integration. (easySDI_4.2.x:240 http://localhost:8080/jenkins/job/easySDI_4.2.x/240/) ........ Auto Commit from continuous integration. (import-DE:14 http://localhost:8080/jenkins/job/import-DE/14/) ........ [jvi]: - set pom files version to 4.2.0-rc-1 ........
    ........
    [jvi]:
    - fix mysql update script
    ........

--- sandboxes/depth/running/mb/4.2.x_gva/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.2.0-rc-1.sql
+++ sandboxes/depth/running/mb/4.2.x_gva/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.2.0-rc-1.sql
@@ -42,7 +42,7 @@ ALTER TABLE #__sdi_order_diffusion MODIFY remark VARCHAR(500) NULL;
 ALTER TABLE #__sdi_order_diffusion MODIFY fee DECIMAL(10) NULL;
 ALTER TABLE #__sdi_order_diffusion MODIFY completed datetime NULL;
 ALTER TABLE #__sdi_order_diffusion MODIFY `file` VARCHAR(500) NULL;
-ALTER TABLE #__sdi_order_diffusion MODIFY `size` DECIMAL(10 NULL;
+ALTER TABLE #__sdi_order_diffusion MODIFY `size` DECIMAL(10) NULL;
 
 UPDATE  #__sdi_searchcriteria SET rendertype_id = 5 WHERE id = 1;
 UPDATE  #__sdi_searchcriteria SET rendertype_id = 2 WHERE id = 2;
