3cddd592b9644163d713eb1ba80dd383b7c9737c
yblatti <yblatti@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-10-31 10:41:35 +0000

    Recreation of 4.2.x branch for ASIT VD

---
+++ sandboxes/asit-vd/4.2.x/com_easysdi_core/src/site/models/resources.php
@@ -0,0 +1,193 @@
+<?php
+
+/**
+ * @version     4.0.0
+ * @package     com_easysdi_core
+ * @copyright   Copyright (C) 2013. All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE.txt
+ * @author      EasySDI Community <contact@easysdi.org> - http://www.easysdi.org
+ */
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.modellist');
+
+/**
+ * Methods supporting a list of Easysdi_core records.
+ */
+class Easysdi_coreModelResources extends JModelList {
+
+    /**
+     * @var sdiUser 
+     */
+    private $user;
+
+    /**
+     * Constructor.
+     *
+     * @param    array    An optional associative array of configuration settings.
+     * @see        JController
+     * @since    1.6
+     */
+    public function __construct($config = array()) {
+        parent::__construct($config);
+
+        $this->user = new sdiUser();
+    }
+
+    /**
+     * Method to auto-populate the model state.
+     *
+     * Note. Calling getState in this method will result in recursion.
+     *
+     * @since	1.6
+     */
+    protected function populateState($ordering = null, $direction = null) {
+
+        // Initialise variables.
+        $app = JFactory::getApplication();
+        
+        // Try to get parentId input
+        $parentid = $app->input->getInt('parentid', NULL);
+        $app->setUserState('com_easysdi_core.parent.resource.version.id', $parentid);
+        
+        // List state information
+        $limit = $app->getUserStateFromRequest('global.list.limit', 'limit', $app->getCfg('list_limit'));
+        $this->setState('list.limit', $limit);
+
+        $limitstart = $app->input->getInt('limitstart', 0);
+        $this->setState('list.start', $limitstart);
+        
+        // Load the filter state.
+        $search = $app->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
+        $this->setState('filter.search', $search);
+
+        $search = $app->getUserStateFromRequest($this->context . '.filter.status', 'filter_status');
+        $this->setState('filter.status', $search);
+
+        $search = $app->getUserStateFromRequest($this->context . '.filter.resourcetype', 'filter_resourcetype');
+        $this->setState('filter.resourcetype', $search);
+        
+        if (empty($ordering)) {
+            $ordering = 'a.ordering';
+        }
+
+        // List state information.
+        parent::populateState($ordering, $direction);
+    }
+
+    /**
+     * Build an SQL query to load the list data.
+     *
+     * @return	JDatabaseQuery
+     * @since	1.6
+     */
+    protected function getListQuery() {
+        // Create a new query object.
+        $lang = JFactory::getLanguage();
+        $db = $this->getDbo();
+        $query = $db->getQuery(true);
+
+        $parentId = JFactory::getApplication()->input->getInt('parentid', null);
+
+        // Select the required fields from the table.
+        $query->select(
+                $this->getState(
+                        'list.select', 'a.*'
+                )
+        );
+
+        $query->from('#__sdi_resource AS a');
+
+        // Join over the users for the checked out user.
+        $query->select('uc.name AS editor');
+        $query->join('LEFT', '#__users AS uc ON uc.id=a.checked_out');
+
+        // Join over the created by field 'created_by'
+        $query->select('created_by.name AS created_by');
+        $query->join('LEFT', '#__users AS created_by ON created_by.id = a.created_by');
+
+        // Join over the foreign key 'resourcetype_id'
+        $query->select('trans.text1 AS resourcetype_name, rt.versioning as versioning, rt.view as supportview, rt.diffusion as supportdiffusion, rt.application as supportapplication');
+        $query->join('LEFT', '#__sdi_resourcetype AS rt ON rt.id = a.resourcetype_id');
+        $query->join('LEFT', '#__sdi_translation AS trans ON trans.element_guid = rt.guid');
+        $query->join('LEFT', '#__sdi_language AS lang ON lang.id = trans.language_id');
+        $query->where('lang.code = ' . $query->quote($lang->getTag()));
+        $query->where('rt.predefined = 0');
+
+        //join over resourcetypelink to know if some relations are possible
+        $query->select('rtl.state as supportrelation');
+        $query->join('LEFT', '(SELECT n.parent_id, state FROM #__sdi_resourcetypelink n GROUP BY n.parent_id) rtl ON rtl.parent_id = rt.id');
+        $query->select('rtl2.state as canbechild');
+        $query->join('LEFT', '(SELECT child_id, state FROM #__sdi_resourcetypelink GROUP BY child_id) rtl2 ON rtl2.child_id=rt.id');
+
+        //join over rights table, check if user have any right on resource
+        $query->innerJoin('#__sdi_user_role_resource AS urr ON urr.resource_id = a.id');
+        $query->where('urr.user_id = ' . $this->user->id);
+
+
+        $query->group('a.id');
+        $query->innerJoin('#__sdi_version v ON v.resource_id = a.id');
+        $query->innerJoin('#__sdi_metadata md ON md.version_id = v.id');
+        
+        if(!empty($parentId)){
+            $query->innerJoin('#__sdi_versionlink vl on v.id = vl.child_id');
+            $query->where('vl.parent_id = ' . (int) $parentId);
+            $this->setState('parentid',$parentId);
+            
+            // Filter by resource type
+            $resourcetype = $this->getState('filter.resourcetype.children');
+            if (is_numeric($resourcetype)) {
+                $query->where('a.resourcetype_id = ' . (int) $resourcetype);
+            }
+
+            // Filter by search in title
+            $search = $this->getState('filter.search.children');
+            if (!empty($search)) {
+                if (stripos($search, 'id:') === 0) {
+                    $query->where('a.id = ' . (int) substr($search, 3));
+                } else {
+                    $search = $db->Quote('%' . $db->escape($search, true) . '%');
+                    $query->where('( a.name LIKE ' . $search . ' )');
+                }
+            }
+
+            //Controls whether a version of the metadata has a status matching the filter.
+            $status = $this->getState('filter.status.children');
+            if (!empty($status)) {
+                $query->where('md.metadatastate_id = ' . $status);
+            }
+        }
+        else{
+            // Filter by resource type
+            $resourcetype = $this->getState('filter.resourcetype');
+            if (is_numeric($resourcetype)) {
+                $query->where('a.resourcetype_id = ' . (int) $resourcetype);
+            }
+
+            // Filter by search in title
+            $search = $this->getState('filter.search');
+            if (!empty($search)) {
+                if (stripos($search, 'id:') === 0) {
+                    $query->where('a.id = ' . (int) substr($search, 3));
+                } else {
+                    $search = $db->Quote('%' . $db->escape($search, true) . '%');
+                    $query->where('( a.name LIKE ' . $search . ' )');
+                }
+            }
+
+            //Controls whether a version of the metadata has a status matching the filter.
+            $status = $this->getState('filter.status');
+            if (!empty($status)) {
+                $query->where('md.metadatastate_id = ' . $status);
+            }
+        }
+        
+        
+        
+        
+        $query->order('a.name');
+
+        return $query;
+    }
+
+}
---
+++ sandboxes/asit-vd/4.2.x/com_easysdi_core/src/site/views/version/tmpl/version.js
@@ -0,0 +1,217 @@
+js = jQuery.noConflict();
+var childrenTable, availablechildrenTable, parents;
+var availablechildrenData = [], childrenData = [], parentsData = [];
+
+var lastCriteria = {length: 0};
+js.fn.dataTableExt.afnFiltering.push(function(oSettings, aData, iDataIndex){
+    return (oSettings.nTable.id != 'sdi-availablechildren' || lastCriteria.length === 0 || (lastCriteria[aData[2]] == aData[3])) ? true : false;
+});
+
+js(document).ready(function() {
+    
+    availablechildrenTable = js('#sdi-availablechildren').dataTable({
+        //"bFilter": false,
+        "bLengthChange": false,
+        "oLanguage": {
+            "sSearch": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SEARCH'),
+            "sZeroRecords": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_NORESULT'),
+            "sInfo": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SHOWING') + " _START_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_TO') + " _END_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_OF') + " _TOTAL_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_RECORDS'),
+            "sInfoEmpty": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SHOWING') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_TO') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_OF') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_RECORDS'),
+            "sInfoFiltered": "(filtered from _MAX_ total records)"
+
+            ,
+            "oPaginate": {
+                "sNext": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_NEXT'),
+                "sPrevious": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_PREVIOUS')
+            }
+        },
+        aaData: availablechildrenData,
+        aoColumnDefs: [
+            { bVisible: false, aTargets: [0], mData: 'id' },
+            { bVisible: false, aTargets: [1], mData: 'guid' },
+            { aTargets: [2], mData: 'resource' },
+            { bVisible: versioning, aTargets: [3], mData: 'version' },
+            { aTargets: [4], mData: 'resourcetype_id', bVisible: false },
+            { aTargets: [5], mData: 'resourcetype' },
+            { aTargets: [6], mData: 'metadatastate_id', bVisible: false },
+            { aTargets: [7], mData: function(child){
+                    return Joomla.JText._(child.state, child.state);
+            }},
+            {
+                aTargets: [8],
+                mData: function(child){
+                    return "<button type='button' id='sdi-availablechildbutton-"+child.id+"' class='btn btn-success btn-mini' onclick='addChild("+JSON.stringify(child)+");'><i class='icon-white icon-new'></i></button>";
+                },
+                sClass: 'center'
+            }
+            
+        ],
+        aaSorting: [[3, 'desc']]
+    });
+    
+    // apply search criteria on available children table
+    js('#jform_searchtype').on('change', function(){
+        availablechildrenTable.fnFilter(this.value, 4);
+    });
+
+    js('#jform_searchid').on('keyup change', function(){
+        availablechildrenTable.fnFilter(this.value, 1);
+    });
+
+    js('#jform_searchname').on('keyup change', function(){
+        availablechildrenTable.fnFilter(this.value, 2);
+    });
+    
+    js('#jform_searchstate').on('change', function(){
+        availablechildrenTable.fnFilter(this.value, 6);
+    });
+    
+    js('input[type=radio][name="jform[searchlast]"]').on('change', function(){
+        if(this.value == 'all'){
+            lastCriteria = {length: 0};
+            availablechildrenTable.fnDraw();
+        }
+        else if(this.value == 'last'){
+            var data = availablechildrenTable.fnGetData();
+            js(data).each(function(i, row){
+                if('undefined' === typeof lastCriteria[row.resource] || row.version > lastCriteria[row.resource]){
+                    lastCriteria[row.resource] = row.version;
+                    lastCriteria.length++;
+                }
+                
+                if(i === js(data).length-1)
+                    availablechildrenTable.fnDraw();
+            });
+        }
+    });
+    
+    // clear criteria
+    js('#clear-btn').on('click', function(){
+        js('#jform_searchtype').val('');
+        availablechildrenTable.fnFilter('', 4);
+        js('#jform_searchid').val('');
+        availablechildrenTable.fnFilter('', 1);
+        js('#jform_searchname').val('');
+        availablechildrenTable.fnFilter('', 2);
+        js('#jform_searchstate').val('');
+        availablechildrenTable.fnFilter('', 6);
+        js('input[type=radio][name="jform[searchlast]"][value=all]').attr('checked', true);
+        lastCriteria = {length: 0};
+        availablechildrenTable.fnFilter('');
+        availablechildrenTable.fnDraw();
+        this.blur();
+        return false;
+    });
+
+    childrenTable = js('#sdi-children').dataTable({
+        "bLengthChange": false,
+        "oLanguage": {
+            "sSearch": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SEARCH'),
+            "sZeroRecords": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_NORESULT'),
+            "sInfo": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SHOWING') + " _START_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_TO') + " _END_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_OF') + " _TOTAL_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_RECORDS'),
+            "sInfoEmpty": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SHOWING') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_TO') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_OF') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_RECORDS'),
+            "sInfoFiltered": "(filtered from _MAX_ total records)"
+
+            ,
+            "oPaginate": {
+                "sNext": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_NEXT'),
+                "sPrevious": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_PREVIOUS')
+            }
+        },
+        aaData: childrenData,
+        aoColumnDefs: [
+            { bVisible: false, aTargets: [0], mData: 'id' },
+            { aTargets: [1], mData: 'resource' },
+            { bVisible: versioning, aTargets: [2], mData: 'version' },
+            { aTargets: [3], mData: 'resourcetype' },
+            { aTargets: [4], mData: function(child){
+                    return Joomla.JText._(child.state, child.state);
+            }},
+            {
+                aTargets: [5],
+                bVisible: !isReadonly,
+                mData: function(child){
+                    return "<button type='button' id='sdi-childbutton-"+child.id+"' class='btn btn-warning btn-mini' onclick='deleteChild("+JSON.stringify(child)+");'><i class='icon-white icon-minus'></i></button>";
+                },
+                sClass: 'center'
+            }
+            
+        ]
+    });
+    
+    parents = js('#sdi-parents').dataTable({
+        "bFilter": true,
+        "bLengthChange": false,
+        "oLanguage": {
+            "sSearch": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SEARCH'),
+            "sZeroRecords": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_NORESULT'),
+            "sInfo": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SHOWING') + " _START_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_TO') + " _END_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_OF') + " _TOTAL_ " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_RECORDS'),
+            "sInfoEmpty": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_SHOWING') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_TO') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_OF') + " 0 " + Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_RECORDS'),
+            "sInfoFiltered": "(filtered from _MAX_ total records)"
+
+            ,
+            "oPaginate": {
+                "sNext": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_NEXT'),
+                "sPrevious": Joomla.JText._('COM_EASYSDI_CORE_DATATABLES_PREVIOUS')
+            }
+        },
+        aaData: parentsData,
+        aoColumnDefs: [
+            { bVisible: false, aTargets: [0], mData: 'id' },
+            { aTargets: [1], mData: 'resource' },
+            { bVisible: versioning, aTargets: [2], mData: 'version' },
+            { aTargets: [3], mData: 'resourcetype' },
+            { aTargets: [4], mData: function(parent){
+                    return Joomla.JText._(parent.state, parent.state);
+            }}
+            
+        ]
+    });
+    
+    /*js('#search-btn').on('click', function(){
+        //var q = [];
+        js('#searchForm :input').each(function(i, input){
+            if('' !== js(input).val()){
+                //q[js(input).attr('name')] = js(input).val();
+                console.log(js(input).attr('name'));
+                switch(js(input).attr('name')){
+                    case 'jform[searchid]':
+                        availablechildrenTable.fnFilter(js(input).val());
+                        break;
+                    
+                    default:
+                        
+                }
+                
+            }
+        });
+        
+        return false;
+    });*/
+});
+
+function addChild(child) {
+    childrenTable.fnAddData(child);
+    availablechildrenTable.fnDeleteRow(js('#sdi-availablechildbutton-' + child.id).parent().parent()[0]);
+}
+
+function deleteChild(child) {
+    availablechildrenTable.fnAddData(child);
+    childrenTable.fnDeleteRow(js('#sdi-childbutton-' + child.id).parent().parent()[0]);
+}
+
+Joomla.submitbutton = function(task)
+{
+    if (task === 'version.save' || task === 'version.apply') {
+        var results = [];
+        var children = childrenTable.fnGetData();
+        children.each(function(value) {
+            results.push(value.id);
+        });
+
+        var r = JSON.stringify(results);
+
+        js('#jform_selectedchildren').val(r);
+    }
+    Joomla.submitform(task, document.getElementById('adminForm'));
+}
