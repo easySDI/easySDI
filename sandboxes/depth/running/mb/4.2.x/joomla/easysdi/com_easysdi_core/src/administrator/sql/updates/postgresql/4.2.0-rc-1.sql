143158faa88ad4dbfcfc712f092f13677e6affe6
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-11-03 10:56:15 +0000

    Create new sandbox

---
+++ sandboxes/depth/running/mb/4.2.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/postgresql/4.2.0-rc-1.sql
@@ -0,0 +1,23 @@
+ALTER TABLE `#__sdi_translation` ADD COLUMN `text3`  character varying(255);
+
+ALTER TABLE `#__sdi_resourcetypelink` DROP CONSTRAINT `#__sdi_resourcetypelink_fk3`;
+ALTER TABLE `#__sdi_resourcetypelink` DROP COLUMN `class_id`;
+
+ALTER TABLE `#__sdi_resourcetypelink` DROP CONSTRAINT `#__sdi_resourcetypelink_fk4`;
+ALTER TABLE `#__sdi_resourcetypelink` DROP COLUMN `attribute_id`;
+
+
+ALTER TABLE `#__sdi_assignment` DROP FOREIGN KEY `#__sdi_assignment_fk3`;
+ALTER TABLE `#__sdi_assignment` CHANGE `version_id` `metadata_id` bigint NOT NULL;
+TRUNCATE `#__sdi_assignment`;
+ALTER TABLE `#__sdi_assignment` ADD CONSTRAINT `#__sdi_assignment_fk3`
+    FOREIGN KEY (`metadata_id` )
+    REFERENCES `#__sdi_metadata` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION;
+
+INSERT INTO `#__sdi_sys_rendertype` VALUES 
+(7, 7, 1, 'datetime'),
+(8, 8, 1, 'gemet');
+
+INSERT INTO `#__sdi_sys_rendertype_stereotype` VALUES ('21', '11', '8');
