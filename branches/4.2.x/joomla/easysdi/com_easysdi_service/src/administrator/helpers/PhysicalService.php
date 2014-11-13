8ed107535375342a6eb88a6cd2e0fb8960b320b2
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-11-13 15:42:34 +0000

    #857 : cURL calls, emptying header 'Expect:'

--- branches/4.2.x/joomla/easysdi/com_easysdi_service/src/administrator/helpers/PhysicalService.php
+++ branches/4.2.x/joomla/easysdi/com_easysdi_service/src/administrator/helpers/PhysicalService.php
@@ -66,9 +66,17 @@ abstract class PhysicalService {
             }
 
             $session = curl_init($completeUrl);
+            $httpHeader = array();
+            // cURL obeys the RFCs as it should. Meaning that for a HTTP/1.1 backend if the POST size is above 1024 bytes
+            // cURL sends a 'Expect: 100-continue' header. The server acknowledges and sends back the '100' status code.
+            // cuRL then sends the request body. This is proper behaviour. Nginx supports this header.
+            // This allows to work around servers that do not support that header.
+            // We're emptying the 'Expect' header, saying to the server: please accept the body right now. 
+            $httpHeader [] = 'Expect:';
             if (!empty($this->user) && !empty($this->password)) {
-                curl_setopt($session, CURLOPT_HTTPHEADER, Array('Authorization: Basic ' . base64_encode($this->user . ':' . $this->password)));
+                $httpHeader[] = 'Authorization: Basic ' . base64_encode($this->user . ':' . $this->password);                
             }
+            curl_setopt($session, CURLOPT_HTTPHEADER, $httpHeader);
             curl_setopt($session, CURLOPT_HEADER, false);
             curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
