2f3999cdc0fce4ac3d83664715297c78836b5b1b
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-11-14 13:10:46 +0000

    Fix table prefix in update script.

--- sandboxes/depth/running/hvh/4.2.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.2.0.sql
+++ sandboxes/depth/running/hvh/4.2.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.2.0.sql
@@ -1,8 +1,8 @@
-ALTER TABLE [slnvp_sdi_translation] DROP FOREIGN KEY [slnvp_sdi_translation_ibfk_1];
-ALTER TABLE [slnvp_sdi_translation] ADD CONSTRAINT [slnvp_sdi_translation_ibfk_1] FOREIGN KEY (`language_id`) REFERENCES `slnvp_sdi_language` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE [#__sdi_translation] DROP FOREIGN KEY [#__sdi_translation_ibfk_1];
+ALTER TABLE [#__sdi_translation] ADD CONSTRAINT [#__sdi_translation_ibfk_1] FOREIGN KEY (`language_id`) REFERENCES `#__sdi_language` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE [slnvp_sdi_translation]  WITH CHECK ADD CONSTRAINT [slnvp_sdi_translation_ibfk_1] FOREIGN KEY([language_id])
-REFERENCES [slnvp_sdi_language] ([id])
+ALTER TABLE [#__sdi_translation]  WITH CHECK ADD CONSTRAINT [#__sdi_translation_ibfk_1] FOREIGN KEY([language_id])
+REFERENCES [#__sdi_language] ([id])
 ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE [slnvp_sdi_translation] CHECK CONSTRAINT [slnvp_sdi_translation_ibfk_1];
+ALTER TABLE [#__sdi_translation] CHECK CONSTRAINT [#__sdi_translation_ibfk_1];
