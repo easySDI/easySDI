f84622acbfb4f7999159c39c21c72ba3a87de5e2
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-11-21 15:29:10 +0000

    Ajout des fichiers sql 4.2.0-rc-2

---
+++ sandboxes/depth/running/mb/4.2.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.2.0-rc-2.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `#__sdi_translation` DROP FOREIGN KEY `#__sdi_translation_ibfk_1`;
+ALTER TABLE `#__sdi_translation` ADD CONSTRAINT `#__sdi_translation_ibfk_1` FOREIGN KEY (`language_id`) REFERENCES `#__sdi_language` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
