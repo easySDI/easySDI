b1815fea38f1935fa384b3f118c092cfab058771
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2014-12-18 14:06:00 +0000

    Merged revision(s) 7840 from branches/4.2.x: [tp] modifs sur le ticket #851 [tp] #851 add 4.2.3 sql files ........

--- sandboxes/depth/running/mb/4.2.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/install/postgresql/insert.sql
+++ sandboxes/depth/running/mb/4.2.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/install/postgresql/insert.sql
@@ -608,7 +608,7 @@ INSERT INTO #__sdi_sys_stereotype (ordering, state, value, defaultpattern, isoco
 INSERT INTO #__sdi_sys_stereotype (ordering, state, value, defaultpattern, isocode, namespace_id, entity_id) VALUES (4, 1, 'number', '^[\-+]?[0-9.]+$', 'Decimal', 2, 1);
 INSERT INTO #__sdi_sys_stereotype (ordering, state, value, defaultpattern, isocode, namespace_id, entity_id) VALUES (5, 1, 'date', '^([0-9]{4}-[0-9]{2}-[0-9]{2})$', 'Date', 2, 1);
 INSERT INTO #__sdi_sys_stereotype (ordering, state, value, defaultpattern, isocode, namespace_id, entity_id) VALUES (6, 1, 'list', '', NULL, NULL, 1);
-INSERT INTO #__sdi_sys_stereotype (ordering, state, value, defaultpattern, isocode, namespace_id, entity_id) VALUES (7, 1, 'link', '^((https?://)?([w.-]+).([a-z.]{2,6})([/w .#:+?%=&;,]*)*/?)$', 'URL', 1, 1);
+INSERT INTO #__sdi_sys_stereotype (ordering, state, value, defaultpattern, isocode, namespace_id, entity_id) VALUES (7, 1, 'link', '((http://|https://|ftp://)(www.)?(([a-zA-Z0-9-]){2,}.){1,4}([a-zA-Z]){2,6}(/([a-zA-Z-_/.0-9#:?=&;,]*)?)?)', 'URL', 1, 1);
 INSERT INTO #__sdi_sys_stereotype (ordering, state, value, defaultpattern, isocode, namespace_id, entity_id) VALUES (8, 1, 'datetime', '^([0-9]{4}-[0-9]{2}-[0-9]{2})$', 'DateTime', 2, 1);
 INSERT INTO #__sdi_sys_stereotype (ordering, state, value, defaultpattern, isocode, namespace_id, entity_id) VALUES (9, 1, 'textchoice', '', 'CharacterString', 2, 1);
 INSERT INTO #__sdi_sys_stereotype (ordering, state, value, defaultpattern, isocode, namespace_id, entity_id) VALUES (10, 1, 'localechoice', '', NULL, NULL, 1);
