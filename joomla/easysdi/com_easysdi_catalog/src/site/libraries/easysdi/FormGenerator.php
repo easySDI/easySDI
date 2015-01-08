c1be817a0e11b98a75c0111017730dd2dd63a667
Marc Battaglia <4466723+marcbat@users.noreply.github.com>
2015-01-08 09:04:01 +0000

    - Support #11335 : Réplication: traduction du status - Défaut #11384: Bouding Box et réplication - Défaut #11389 : Certain Search Criteria not displayed

--- joomla/easysdi/com_easysdi_catalog/src/site/libraries/easysdi/FormGenerator.php
+++ joomla/easysdi/com_easysdi_catalog/src/site/libraries/easysdi/FormGenerator.php
@@ -90,7 +90,7 @@ class FormGenerator {
      * @version 4.0.0
      */
     public function getForm() {
-
+        
         if (!isset($_GET['relid'])) {
             $query = $this->db->getQuery(true);
             $query->select('r.id, r.name, r.childtype_id');
@@ -321,7 +321,7 @@ class FormGenerator {
         $formStereotype = new FormStereotype();
 
         $query = $this->getRelationQuery();
-        $query->where('r.parent_id = ' . $parent_id);
+        $query->where('r.parent_id = ' . $query->quote($parent_id));
         $query->where('r.state = 1');
         $query->order('r.ordering');
         $query->order('r.name');
@@ -457,6 +457,10 @@ class FormGenerator {
         return $element;
     }
 
+    /**
+     * Clone structure and remove from the clone node that not in csw domdocument and get the value from the csw
+     * 
+     */
     private function cleanStructure() {
         //clone the structure - having a document between the structure and the csw let us do the bi-directional merge
         $clone_structure = new DOMDocument('1.0', 'utf-8');
@@ -504,9 +508,19 @@ class FormGenerator {
         $this->mergeToStructure($clone_structure, $domXpathClone);
     }
 
+    /**
+     * Merge structure clone to original structure
+     * 
+     * @param DOMDocument $clone
+     * @param DOMXPath $domXpathClone
+     * 
+     */
     private function mergeToStructure(DOMDocument $clone, DOMXPath $domXpathClone) {
         /* @var $node DOMElement */
         foreach ($this->domXpathStr->query('//*[@catalog:childtypeId="' . EnumChildtype::$CLASS . '"]|//*[@catalog:childtypeId="' . EnumChildtype::$ATTRIBUT . '"]') as $node) {
+            if(strpos($node->getNodePath(), 'gmd:extent') > -1 ){
+                $breakpoint = true; 
+            }
             if ($domXpathClone->query($node->getNodePath())->length == 0) {
                 do {
                     $childToImport = $node;
@@ -767,7 +781,6 @@ class FormGenerator {
         $relId = $attribute->getAttributeNS($this->catalog_uri, 'relid');
         $guid = $attribute->getAttributeNS($this->catalog_uri, 'relGuid');
         $label = $attribute->getAttributeNS($this->catalog_uri, 'label');
-        $boundingbox = $attribute->getAttributeNS($this->catalog_uri, 'boundingbox');
 
         $fields = array();
         $field = $this->form->createElement('field');
@@ -1437,27 +1450,23 @@ class FormGenerator {
      * @since 4.0.0
      */
     private function getValidatorClass(DOMElement $attribute) {
-        $validator = '';
+        $validator = array();
         $guid = $attribute->getAttributeNS($this->catalog_uri, 'id');
         $patterns = $this->getPatterns();
 
         if ($attribute->getAttributeNS($this->catalog_uri, 'lowerbound') > 0) {
-            $validator .= ' required ';
+            $validator[] = 'required';
         }
 
         if (array_key_exists($guid, $patterns)) {
             if ($patterns[$guid]->attribute_pattern != '') {
-                $validator .= ' validate-sdi' . $patterns[$guid]->guid;
+                $validator[] = 'validate-sdi' . $patterns[$guid]->guid;
             } elseif ($patterns[$guid]->stereotype_pattern != '') {
-                $validator .= ' validate-sdi' . $patterns[$guid]->stereotype_name;
+                $validator[] = 'validate-sdi' . $patterns[$guid]->stereotype_name;
             }
-
-            return $validator;
-        } elseif ($attribute->getAttributeNS($this->catalog_uri, 'childtypeId') == EnumChildtype::$RELATIONTYPE) {
-            return $validator;
-        } else {
-            return '';
         }
+        
+        return implode(' ',$validator);
     }
 
     /**
