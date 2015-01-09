b5ab8604fc7965ce5fe926b2d323c31947063179
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-01-09 08:05:16 +0000

    Défaut #11383 : Synchronisation: message d'avertissement

--- branches/4.2.x/joomla/easysdi/com_easysdi_core/src/site/views/resources/tmpl/default.php
+++ branches/4.2.x/joomla/easysdi/com_easysdi_core/src/site/views/resources/tmpl/default.php
@@ -257,6 +257,25 @@ $document = JFactory::getDocument();
     </div>
 </div>
 
+<!-- synchronize modal -->
+<div class="modal fade" id="synchronizeModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel"><?php echo JText::_('COM_EASYSDI_CORE_SYNCHRONIZE_TITLE'); ?></h4>
+            </div>
+            <div id="synchronizeModalBody" class="modal-body">
+                <?php echo JText::_('COM_EASYSDI_CORE_SYNCHRONIZE_CONFIRM'); ?>
+            </div>
+            <div class="modal-footer">
+                <a href="#" id="btn_synchronize"><button type="button" class="btn btn-success"><?php echo JText::_('COM_EASYSDI_CORE_SYNCHRONIZE_TITLE'); ?></button></a>
+                <button type="button" class="btn btn-danger" data-dismiss="modal"><?php echo JText::_('JCANCEL'); ?></button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <!-- Assignment Modal -->
 <div class="modal fade" id="assignmentModal" tabindex="-1" role="dialog" aria-labelledby="assignmentModalLabel" aria-hidden="true">
     <form id="form_assign" action="<?php echo JRoute::_('index.php?option=com_easysdi_catalog&task=metadata.assign'); ?>" method="post" class="form-validate form-horizontal">
