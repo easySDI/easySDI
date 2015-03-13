6b5bc2df3ca7b209f7f688b4eb024106a3378dbf
yblatti <yblatti@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-03-13 16:43:24 +0000

    #898 (component SHOP -> pricing)

--- sandboxes/asit-vd/4.3.x/com_easysdi_shop/src/site/views/pricingprofile/tmpl/default.php
+++ sandboxes/asit-vd/4.3.x/com_easysdi_shop/src/site/views/pricingprofile/tmpl/default.php
@@ -16,12 +16,11 @@ JHtml::_('behavior.formvalidation');
 
 $document = JFactory::getDocument();
 $document->addScript('components/com_easysdi_shop/helpers/helper.js');
-
 ?>
 
 <script type="text/javascript">
-    jQuery(document).ready(function(){
-        Joomla.submitbutton = function(task){
+    jQuery(document).ready(function () {
+        Joomla.submitbutton = function (task) {
             taskArray = task.split('.');
             jQuery('input[name=action]').val(taskArray[1]);
             Joomla.submitform(task, document.getElementById('adminForm'));
@@ -30,50 +29,55 @@ $document->addScript('components/com_easysdi_shop/helpers/helper.js');
 </script>
 
 <div class="shop front-end-edit">
-    <h1><?php echo JText::_('COM_EASYSDI_SHOP_PRICINGORGANISM_TITLE'); ?> :</h1>
-    
+    <h1><?php echo JText::_('COM_EASYSDI_SHOP_PRICINGORGANISM_TITLE') . ' : ' . $this->item->name; ?></h1>
+
     <div class="well">
-    
+
         <form class="form-inline form-validate" action="<?php echo JRoute::_('index.php?option=com_easysdi_shop&view=pricingorganism'); ?>" method="post" id="adminForm" name="adminForm" enctype="multipart/form-data">
-            
-            <?php foreach($this->form->getFieldsets() as $fieldset): ?>
-                <?php if(isset($fieldset->label)):?><h4><?php echo JText::_($fieldset->label);?></h4><?php endif;?>
+            <?php foreach ($this->form->getFieldsets() as $fieldset): ?>
+                <?php if (isset($fieldset->label)): ?><h4><?php echo JText::_($fieldset->label); ?></h4><?php endif; ?>
                 <table>
-                    <?php if($fieldset->name == 'free_categories'):?>
+                    <?php if ($fieldset->name == 'free_categories'): ?>
                         <thead>
                             <tr>
-                                <th><?php echo JText::_('COM_EASYSDI_SHOP_PRICINGPROFILE_TH_CATEGORIES');?></th>
-                                <th><?php echo JText::_('COM_EASYSDI_SHOP_PRICINGPROFILE_TH_IS_FREE');?></th>
+                                <th><?php echo JText::_('COM_EASYSDI_SHOP_PRICINGPROFILE_TH_CATEGORIES'); ?></th>
+                                <th><?php echo JText::_('COM_EASYSDI_SHOP_PRICINGPROFILE_TH_IS_FREE'); ?></th>
                             </tr>
                         </thead>
                         <tbody>
-                        <?php foreach($this->item->categories as $category):?>
-                            <tr>
-                                <td><?php echo $category->name; ?></td>
-                                <td>
-                                    <input type="radio" name="jform[categories][<?php echo $category->id;?>]" value="1" <?php if($category->isFree>0):?>checked<?php endif;?>><label><?php echo JText::_('YES'); ?></label>
-                                    <input type="radio" name="jform[categories][<?php echo $category->id;?>]" value="0" <?php if($category->isFree==0):?>checked<?php endif;?>><label><?php echo JText::_('NO'); ?></label>
-                                </td>
-                            </tr>
-
-                        <?php endforeach;?>
+                            <?php foreach ($this->item->categories as $category): ?>
+                                <tr>
+                                    <td><?php echo $category->name; ?></td>
+                                    <td>
+                                        <fieldset id="jform_categories_<?php echo $category->id; ?>_free" class="radio btn-group btn-group-yesno">
+                                            <input type="radio" id="jform_categories_<?php echo $category->id; ?>_free0" name="jform[categories][<?php echo $category->id; ?>]" value="0" <?php if ($category->isFree == 0): ?>checked="checked"<?php endif; ?>>
+                                            <label for="jform_categories_<?php echo $category->id; ?>_free0"><?php echo JText::_('JNO'); ?></label>                                            
+                                            <input type="radio" id="jform_categories_<?php echo $category->id; ?>_free1" name="jform[categories][<?php echo $category->id; ?>]" value="1" <?php if ($category->isFree > 0): ?>checked="checked"<?php endif; ?>>
+                                            <label for="jform_categories_<?php echo $category->id; ?>_free1"><?php echo JText::_('JYES'); ?></label>
+                                        </fieldset>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
                         </tbody></table>
-                    <?php break; endif;?>
-                    
-                    <?php foreach($this->form->getFieldset($fieldset->name) as $field): ?>
+                    <?php
+                    break;
+                endif;
+                ?>
+
+    <?php foreach ($this->form->getFieldset($fieldset->name) as $field): ?>
                     <tr>
                         <td><?php echo $field->label; ?></td>
-                        <td><?php echo $field->input; ?> <?php echo $field->name=="jform[name]" ? "" : ($field->name=="jform[surface_rate]" ? "km2" : $this->paramsarray["currency"]); ?></td>
+                        <td><?php echo $field->input; ?> <?php echo $field->name == "jform[name]" ? "" : ($field->name == "jform[surface_rate]" ? "km2" : $this->paramsarray["currency"]); ?></td>
                     </tr>
-                    <?php endforeach; ?>
+                <?php endforeach; ?>
                 </table>
-            <?php endforeach; ?>
-                
-            <input type="hidden" name="task" id="task" value="" />
-            <input type="hidden" name="id" value="<?php echo $this->item->id;?>" />
-            <input type="hidden" name="organism_id" value="<?php echo $this->state->get('pricingprofile.organism_id');?>" />
+<?php endforeach; ?>
 
-            <?php echo $this->getToolbar(); ?>
+            <input type="hidden" name="task" id="task" value="" />
+            <input type="hidden" name="id" value="<?php echo $this->item->id; ?>" />
+            <input type="hidden" name="organism_id" value="<?php echo $this->state->get('pricingprofile.organism_id'); ?>" />
+
+<?php echo $this->getToolbar(); ?>
         </form>
     </div>
 </div>
