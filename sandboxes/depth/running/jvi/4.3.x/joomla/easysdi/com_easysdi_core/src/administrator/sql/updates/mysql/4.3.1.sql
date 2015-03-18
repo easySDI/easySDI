cbd9876ce310e095be415bb1a9b9424be3c6dae5
jvillemagne <jvillemagne@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-03-18 15:51:47 +0000

    [jvi - Refs #939]:     - add storage_id field in sdi_order_diffusion table (install/alter/update - mysql/pgsql/sqlserver)

---
+++ sandboxes/depth/running/jvi/4.3.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.3.1.sql
@@ -0,0 +1,4 @@
+
+
+ALTER TABLE `#__sdi_order_diffusion` ADD `storage_id` INT(11) UNSIGNED NOT NULL AFTER completed;
+ALTER TABLE `#__sdi_order_diffusion` ADD CONSTRAINT `#__sdi_order_diffusion_fk4` FOREIGN KEY (`storage_id`) REFERENCES `#__sdi_sys_extractstorage` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
