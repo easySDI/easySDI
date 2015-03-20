3d4942ee68bc2317aa1aa498dfb9a72a5aa3950e
yblatti <yblatti@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-03-20 14:24:43 +0000

    Merged revision(s) 8325-8333 from branches/4.3.x/joomla/easysdi: Auto Commit from continuous integration. (easySDI_4.3.x:72 http://localhost:8080/jenkins/job/easySDI_4.3.x/72/) ........ [jvi]:     - add storage_id field in sdi_order_diffusion table (install/alter/update - mysql/pgsql/sqlserver)     - cast pricing data to build getOrders response xml     - fix fields on which restrictionList will match against     - setting easySDI version to 4.3.1
    ........
    [jvi]:
        - set storage_id value for existing order_diffusion
    ........

---
+++ sandboxes/asit-vd/4.3.x/com_easysdi_core/src/administrator/sql/updates/postgresql/4.3.1.sql
@@ -0,0 +1,7 @@
+
+
+ALTER TABLE #__sdi_order_diffusion ADD storage_id bigint NOT NULL AFTER completed;
+UPDATE #__sdi_order_diffusion SET storage_id = 1;
+
+ALTER TABLE ONLY #__sdi_order_diffusion
+    ADD CONSTRAINT #__sdi_order_diffusion_fk4 FOREIGN KEY (storage_id) REFERENCES #__sdi_sys_extractstorage(id) MATCH FULL;
