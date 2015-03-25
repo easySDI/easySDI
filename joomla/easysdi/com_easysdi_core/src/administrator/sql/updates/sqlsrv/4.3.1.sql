a921097d89d2ae566c5fde238965c45658afe7cb
Jérôme VILLEMAGNE <21275566+jvillemagne@users.noreply.github.com>
2015-03-25 08:23:11 +0000

    [jvi]:     - Refs #939: fix 4.3.1 sql (+ installers)

--- joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.3.1.sql
+++ joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.3.1.sql
@@ -1,8 +1,6 @@
 
 
-ALTER TABLE [#__sdi_order_diffusion] ADD [storage_id] [bigint] NOT NULL;
-UPDATE [#__sdi_order_diffusion] SET [storage_id] = 1;
-
+ALTER TABLE [#__sdi_order_diffusion] ADD [storage_id] [bigint] NULL;
 ALTER TABLE [#__sdi_order_diffusion]  WITH NOCHECK ADD  CONSTRAINT [#__sdi_order_diffusion$#__sdi_order_diffusion_fk4] FOREIGN KEY([storage_id])
 REFERENCES [#__sdi_sys_extractstorage] ([id])
 ON DELETE NO ACTION
