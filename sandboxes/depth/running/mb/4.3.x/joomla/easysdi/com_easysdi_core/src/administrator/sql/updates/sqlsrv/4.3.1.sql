593cb9940a96158aa6d4e36bfd1600e781cea8de
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-03-27 10:37:12 +0000

    create sb 4.3.x

---
+++ sandboxes/depth/running/mb/4.3.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.3.1.sql
@@ -0,0 +1,7 @@
+
+
+ALTER TABLE [#__sdi_order_diffusion] ADD [storage_id] [bigint] NULL;
+ALTER TABLE [#__sdi_order_diffusion]  WITH NOCHECK ADD  CONSTRAINT [#__sdi_order_diffusion$#__sdi_order_diffusion_fk4] FOREIGN KEY([storage_id])
+REFERENCES [#__sdi_sys_extractstorage] ([id])
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
