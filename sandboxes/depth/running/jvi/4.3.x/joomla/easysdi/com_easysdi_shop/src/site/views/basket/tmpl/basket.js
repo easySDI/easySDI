67dc14916c66b9f0c18d4eba76065882569b11e9
jvillemagne <jvillemagne@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-04-15 16:08:18 +0000

    [jvi - Refs #11620]:     - Fix javascript basket's tooltip changes

--- sandboxes/depth/running/jvi/4.3.x/joomla/easysdi/com_easysdi_shop/src/site/views/basket/tmpl/basket.js
+++ sandboxes/depth/running/jvi/4.3.x/joomla/easysdi/com_easysdi_shop/src/site/views/basket/tmpl/basket.js
@@ -268,14 +268,15 @@ var updatePricing = function(pricing) {
                         discount = profileDiscount;
                     }
                     title = Joomla.JText._('COM_EASYSDI_SHOP_BASKET_TOOLTIP_REBATE_INFO', 'As %s, you get a discount of %s%%').replace(/(.*)(?:%s)(.*)(?:%s%)(.*)/gi, '$1'+as+'$2'+discount+'$3');
-                    jQuery('table[rel=' + supplierId + ']>tbody>tr[rel=' + productId + ']>td.price_column>i.icon-info').attr('data-original-title', title).show();
+                    jQuery('table[rel=' + supplierId + ']>tbody>tr[rel=' + productId + ']>td.price_column>i.icon-info').attr('title', title).show();
                 }
                 else{
-                    jQuery('table[rel=' + supplierId + ']>tbody>tr[rel=' + productId + ']>td.price_column>i.icon-info').attr('data-original-title', title).hide();
+                    jQuery('table[rel=' + supplierId + ']>tbody>tr[rel=' + productId + ']>td.price_column>i.icon-info').attr('title', title).hide();
                 }
             }
             var i = jQuery('table[rel=' + supplierId + ']>tbody>tr[rel=' + productId + ']>td.price_column>i.icon-info');
             jQuery('table[rel=' + supplierId + ']>tbody>tr[rel=' + productId + ']>td.price_column').html(displayedPrice+' ').append(i).show();
+            jQuery('table[rel=' + supplierId + ']>tbody>tr[rel=' + productId + ']>td.price_column>i.icon-info').tooltip({"html": true,"container": "body"});
         });
 
         //footer
