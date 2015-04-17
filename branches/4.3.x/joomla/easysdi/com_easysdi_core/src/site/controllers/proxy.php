11383a59ecfe8e196de7541ed3a7c23c1813447d
jvillemagne <jvillemagne@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-04-17 13:57:29 +0000

    [jvi]:     - Refs #964: url test does not work     - Refs #962: fix button ids     - Refs #11620: fix js basket refresh

--- branches/4.3.x/joomla/easysdi/com_easysdi_core/src/site/controllers/proxy.php
+++ branches/4.3.x/joomla/easysdi/com_easysdi_core/src/site/controllers/proxy.php
@@ -11,168 +11,43 @@
 defined('_JEXEC') or die;
 
 require_once JPATH_COMPONENT . '/controller.php';
+require_once JPATH_ADMINISTRATOR.'/components/com_easysdi_core/helpers/curl.php';
 
 /**
  * Resource controller class.
  */
 class Easysdi_coreControllerProxy extends Easysdi_coreController{
     
-    private $method;
-    private $post = array();
-    private $get = array();
-    private $cookies = array();
+    private $curlHelper;
+    private $inputs;
     
-    private $url;
-    private $option;
-    protected $task;
-    private $response;
+    public function __construct($config = array()) {
+        parent::__construct($config);
+        $this->curlHelper = new CurlHelper();
+        $this->inputs = JFactory::getApplication()->input;
+    }
     
     public function run(){
-        
-        $jInput = JFactory::getApplication()->input;
-        
-        $this->method = $jInput->getMethod();
-        
-        $this->getGETParameters($jInput);
-        $this->getPOSTParameters($jInput);
-        $this->getCookies($jInput);
-        
-        $this->prepareURL();
-        
-        $this->sendCURLRequest();
-        
-        $this->sendResponse();
+        $this->curlHelper->run($this->inputs);
     }
     
-    private function getGETParameters(JInput $jInput){
-        $data = $jInput->get->getArray();
-        
-        if(isset($data['option'])){
-            $this->option = $data['option'];
-            unset($data['option']);
-        }
-        
-        if(isset($data['task'])){
-            $this->task = $data['task'];
-            unset($data['task']);
-        }
-        
-        if(isset($data['url'])){
-            $this->url = $data['url'];
-            unset($data['url']);
-        }
-        
-        if(isset($data['method'])){
-            $this->method = $data['method'];
-            unset($data['method']);
-        }
-        
-        if(count($data)){
-            $query = array();
-            foreach($data as $key => $value){
-                array_push($query, $key.'='.$value);
-            }
-            $this->get = implode('&', $query);
-        }
+    public function getRequest(){
+        $this->curlHelper->get($this->inputs);
     }
     
-    private function getPOSTParameters(JInput $jInput){
-        $data = $jInput->post->getArray();
-        
-        if(empty($data)){
-            if($this->method == 'POST'){
-                $this->post = file_get_contents("php://input");
-            }
-            return;
-        }
-        
-        if(isset($data['url'])){
-            $this->url = $data['url'];
-            unset($data['url']);
-        }
-        
-        if(isset($data['method'])){
-            $this->method = $data['method'];
-            unset($data['method']);
-        }
-        
-        if(count($data)){
-            $query = array();
-            foreach($data as $key => $value){
-                array_push($query, $key.'='.$value);
-            }
-            $this->post = implode('&', $query);
-        }
+    public function head(){
+        $this->curlHelper->head($this->inputs);
     }
     
-    private function getCookies(JInput $jInput){
-        $data = array();
-        foreach($jInput->cookie->getArray() as $cookieName => $cookieValue){
-            array_push($data, $cookieName.'='.$cookieValue);
-        }
-        $this->cookies = implode('; ', $data);
+    public function post(){
+        $this->curlHelper->post($this->inputs);
     }
     
-    private function prepareURL(){
-        if(count($this->get)){
-            $this->url .= '?'.$this->get;
-        }
+    public function put(){
+        $this->curlHelper->put($this->inputs);
     }
     
-    private function sendCURLRequest(){
-        $ch = curl_init($this->url);
-        
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml; charset="UTF-8"', 'charset="UTF-8"','Expect:'));
-        
-        curl_setopt($ch, CURLOPT_COOKIE, $cookies);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_ENCODING, "");
-        
-        switch($this->method){
-            case 'POST':
-                $this->sendPOSTCURLRequest($ch);
-                break;
-            
-            case 'GET':
-            default:
-                $this->sendGETCURLRequest($ch);
-        }
-        
-        $response = new stdClass();
-        $response->content = curl_exec($ch);
-        $response->data = curl_getinfo($ch);
-        $this->response = $response;
-        
-        curl_close($ch);
-    }
-    
-    private function sendGETCURLRequest(&$ch){
-        curl_setopt($ch, CURLOPT_POST, 0);
-    }
-    
-    private function sendPOSTCURLRequest(&$ch){
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->post);
-    }
-    
-    private function sendResponse(){
-        // if no content type defined, set as text/plain
-        if($this->response->data['content_type'] == ''){
-            $this->response->data['content_type'] = 'text/plain';
-        }
-        
-        // if no charset defined, force utf-8
-        if(strpos($this->response->data['content_type'], 'charset') == false){
-            list($ct, $trash) = explode(';', $this->response->data['content_type']);
-            $this->response->data['content_type'] = trim($ct).'; charset=utf-8';
-        }
-        
-        // set headers then output response
-        header('Content-Type: '.$this->response->data['content_type']);
-        echo trim($this->response->content);
-        die();
+    public function delete(){
+        $this->curlHelper->delete($this->inputs);
     }
 }
