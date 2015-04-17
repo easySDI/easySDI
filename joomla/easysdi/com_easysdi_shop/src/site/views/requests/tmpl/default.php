54d56f546c781bb654dfa432fa93a622f788d4e9
Jérôme VILLEMAGNE <21275566+jvillemagne@users.noreply.github.com>
2015-04-17 08:50:54 +0000

    [jvi - from YB]:     - Fix #955 : html titles levels and forms formatted by tables     - Fix #956 : Missing CSS class for search/filters/actions top bars : "sdi-searchcriteria"

--- joomla/easysdi/com_easysdi_shop/src/site/views/requests/tmpl/default.php
+++ joomla/easysdi/com_easysdi_shop/src/site/views/requests/tmpl/default.php
@@ -16,7 +16,7 @@ JHtml::_('formbehavior.chosen', 'select');
 ?>
 <div class="shop front-end-edit">
     <h1><?php echo JText::_('COM_EASYSDI_SHOP_TITLE_REQUESTS'); ?></h1>
-    <div class="well">
+    <div class="well sdi-searchcriteria">
         <div class="row-fluid">
             <form class="form-search" action="<?php echo JRoute::_('index.php?option=com_easysdi_shop&view=requests'); ?>" method="post">
                 <div class="btn-toolbar">
