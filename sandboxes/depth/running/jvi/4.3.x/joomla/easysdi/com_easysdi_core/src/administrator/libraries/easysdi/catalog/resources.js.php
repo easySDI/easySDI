5cfcc76bbc160dc7d9b4af78fc196ee46fa5a527
jvillemagne <jvillemagne@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-05-18 14:46:22 +0000

    [jvi - Refs #895]:     - fix Metadata edition

--- sandboxes/depth/running/jvi/4.3.x/joomla/easysdi/com_easysdi_core/src/administrator/libraries/easysdi/catalog/resources.js.php
+++ sandboxes/depth/running/jvi/4.3.x/joomla/easysdi/com_easysdi_core/src/administrator/libraries/easysdi/catalog/resources.js.php
@@ -363,7 +363,7 @@ var buildMetadataDropDown = function(resource){
     if( 
         (resource.rights.metadataEditor && metadata.state===metadataState.INPROGRESS)
         ||
-        (resource.rights.metadataResponsible && js.inArray(metadata.state, [metadataState.INPROGRESS, metadataState.VALIDATED, metadataState.PUBLISHED])===true)
+        (resource.rights.metadataResponsible && js.inArray(metadata.state, [metadataState.INPROGRESS, metadataState.VALIDATED, metadataState.PUBLISHED])>-1)
         || resource.rights.organismManager
     ){
         section.push(buildDropDownItem(resource, 'metadata.edit'));
