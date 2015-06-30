f65481c58a51628c0f51a79f0141ae15db170d11
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-06-30 13:35:42 +0000

    #1050 : SHOP Pricing does not support "large" amounts - SQL Server fixes

---
+++ branches/4.3.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/Procedure-4.3.2-__sdi_pricing_order_supplier_product_profile.sql
@@ -0,0 +1,25 @@
+DECLARE @SchemaName NVARCHAR(256),
+        @TableName NVARCHAR(256),
+        @SQL NVARCHAR(MAX),
+        @NewLine CHAR(1)
+ 
+SELECT  @SchemaName = N'dbo',
+        @TableName = N'sdi_sdi_pricing_order_supplier_product_profile',
+        @NewLine = CHAR(10)
+ 
+SELECT  @SQL = ISNULL(@SQL + @NewLine, '') + 
+            'ALTER TABLE [' + S.name + '].[' + T.name + '] ' +
+                'DROP CONSTRAINT [' + D.name + ']'
+FROM    sys.tables T
+    INNER JOIN sys.default_constraints D
+        ON D.parent_object_id = T.object_id
+    INNER JOIN sys.columns C
+        ON C.object_id = T.object_id
+            AND C.column_id = D.parent_column_id
+    INNER JOIN sys.schemas S
+        ON T.schema_id = S.schema_id
+WHERE   S.name = @SchemaName
+  AND   T.name = @TableName
+ 
+PRINT @SQL
+EXECUTE (@SQL)
