e0e6077a35023e4b060f25f9af657d7a3f956947
Hélène Van Hoecke <2844416+hvanhoecke@users.noreply.github.com>
2015-06-30 13:35:42 +0000

    #1050 : SHOP Pricing does not support "large" amounts - SQL Server fixes

--- joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.3.2.sql
+++ joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.3.2.sql
@@ -22,31 +22,35 @@ ALTER TABLE [#__sdi_relation] ALTER COLUMN [accessscope_limitation] [tinyint] ;
 
 ALTER TABLE [#__sdi_order] ADD [freeperimetertool] [nvarchar](100) NULL;
 
+ALTER TABLE [#__sdi_pricing_profile] ALTER COLUMN [fixed_fee]  decimal(19,2) NULL;
+ALTER TABLE [#__sdi_pricing_profile]  ALTER COLUMN [surface_rate]  decimal(19,2) NULL;
+ALTER TABLE [#__sdi_pricing_profile] ALTER COLUMN [min_fee]  decimal(19,2) NULL ;
+ALTER TABLE [#__sdi_pricing_profile]  ALTER COLUMN [max_fee]  decimal(19,2) NULL;
 
-ALTER TABLE [#__sdi_pricing_profile]
-ALTER COLUMN [fixed_fee]  decimal(19,2) NULL,
-ALTER COLUMN [surface_rate]  decimal(19,2) NULL,
-ALTER COLUMN [min_fee]  decimal(19,2) NULL,
-ALTER COLUMN [max_fee]  decimal(19,2) NULL;
+ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ALTER COLUMN [cfg_fixed_fee]  decimal(19,2) NOT NULL;
+ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ADD CONSTRAINT DF_cfg_fixed_fee DEFAULT 0.00 FOR [cfg_fixed_fee];
+ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ALTER COLUMN [cfg_surface_rate]  decimal(19,2) NOT NULL;
+ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ADD CONSTRAINT DF_cfg_surface_rate DEFAULT 0.00 FOR [cfg_surface_rate];
+ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ALTER COLUMN [cfg_min_fee]  decimal(19,2) NOT NULL ;
+ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ADD CONSTRAINT DF_cfg_min_fee DEFAULT 0.00 FOR [cfg_min_fee];
+ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ALTER COLUMN [cfg_max_fee]  decimal(19,2) NOT NULL ;
+ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ADD CONSTRAINT DF_cfg_max_fee DEFAULT 0.00 FOR [cfg_max_fee];
+ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ALTER COLUMN [cfg_pct_category_profile_discount]  decimal(19,2) NOT NULL ;
+ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ADD CONSTRAINT DF_cfg_pct_cpd DEFAULT 0.00 FOR [cfg_pct_category_profile_discount];
 
-ALTER TABLE [#__sdi_pricing_order_supplier_product_profile]
-ALTER COLUMN [cfg_fixed_fee]  decimal(19,2) NOT NULL DEFAULT 0.00,
-ALTER COLUMN [cfg_surface_rate]  decimal(19,2) NOT NULL DEFAULT 0.00,
-ALTER COLUMN [cfg_min_fee]  decimal(19,2) NOT NULL DEFAULT 0.00,
-ALTER COLUMN [cfg_max_fee]  decimal(19,2) NOT NULL DEFAULT 0.00,
-ALTER COLUMN [cfg_pct_category_profile_discount]  decimal(19,2) NOT NULL DEFAULT 0.00;
+ALTER TABLE [#__sdi_pricing_order_supplier_product] ALTER COLUMN [cfg_pct_category_supplier_discount]  decimal(19,2) NOT NULL;
+ALTER TABLE [#__sdi_pricing_order_supplier_product] ADD CONSTRAINT DF_cfg_pct_csd DEFAULT 0.00 FOR [cfg_pct_category_supplier_discount];
 
-ALTER TABLE [#__sdi_pricing_order_supplier_product]
-ALTER COLUMN [cfg_pct_category_supplier_discount]  decimal(19,2) NOT NULL DEFAULT 0.00;
+ALTER TABLE [#__sdi_pricing_order_supplier] ALTER COLUMN [cfg_fixed_fee_ti]  decimal(19,2) NOT NULL;
+ALTER TABLE [#__sdi_pricing_order_supplier] ADD CONSTRAINT DF_cfg_fixed_fee_ti DEFAULT 0.00 FOR [cfg_fixed_fee_ti];
+ALTER TABLE [#__sdi_pricing_order_supplier] ALTER COLUMN [cal_fee_ti]  decimal(19,2) NOT NULL;
+ALTER TABLE [#__sdi_pricing_order_supplier] ADD CONSTRAINT DF_cal_fee_ti DEFAULT 0.00 FOR [cal_fee_ti];
 
-ALTER TABLE [#__sdi_pricing_order_supplier]
-ALTER COLUMN [cfg_fixed_fee_ti]  decimal(19,2) NOT NULL DEFAULT 0.00,
-ALTER COLUMN [cal_fee_ti]  decimal(19,2) NOT NULL DEFAULT 0.00;
+ALTER TABLE [#__sdi_pricing_order] ALTER COLUMN [cfg_vat]  decimal(19,2) NOT NULL;
+ALTER TABLE [#__sdi_pricing_order] ADD CONSTRAINT DF_cfg_vat DEFAULT 0.00 FOR [cfg_vat];
+ALTER TABLE [#__sdi_pricing_order] ALTER COLUMN [cfg_overall_default_fee]  decimal(19,2) NOT NULL;
+ALTER TABLE [#__sdi_pricing_order] ADD CONSTRAINT DF_cfg_overall_default_fee DEFAULT 0.00 FOR [cfg_overall_default_fee];
+ALTER TABLE [#__sdi_pricing_order] ALTER COLUMN [cal_fee_ti]  decimal(19,2) NOT NULL;
+ALTER TABLE [#__sdi_pricing_order] ADD CONSTRAINT DF_cal_fee_ti_po DEFAULT 0.00 FOR [cal_fee_ti];
 
-ALTER TABLE [#__sdi_pricing_order]
-ALTER COLUMN [cfg_vat]  decimal(19,2) NOT NULL DEFAULT 0.00,
-ALTER COLUMN [cfg_overall_default_fee]  decimal(19,2) NOT NULL DEFAULT 0.00,
-ALTER COLUMN [cal_fee_ti]  decimal(19,2) NOT NULL DEFAULT 0.00;
-
-ALTER TABLE [#__sdi_organism_category_pricing_rebate]
-ALTER COLUMN [rebate]  decimal(19,2) NULL;
+ALTER TABLE [#__sdi_organism_category_pricing_rebate] ALTER COLUMN [rebate]  decimal(19,2) NULL;
