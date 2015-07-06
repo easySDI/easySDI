3a1dc4ec45ce412f2a4035e7d1f572b6b6663c5d
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-07-06 11:21:49 +0000

    #1021 : Upgrade crashes after re install

--- branches/4.3.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.3.0-beta-4.sql
+++ branches/4.3.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.3.0-beta-4.sql
@@ -1,4 +1,6 @@
+CALL drop_column('sdi_maplayer', 'levelfield');
 ALTER TABLE `#__sdi_maplayer` ADD COLUMN `levelfield`  varchar(255) NULL AFTER `asOLoptions`;
+CALL drop_column('sdi_maplayer', 'isindoor');
 ALTER TABLE `#__sdi_maplayer` ADD COLUMN `isindoor`  TINYINT(1) NULL AFTER `asOLoptions`;
 
 CREATE TABLE IF NOT EXISTS `#__sdi_sys_server` (
@@ -9,8 +11,8 @@ CREATE TABLE IF NOT EXISTS `#__sdi_sys_server` (
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT COLLATE=utf8_general_ci;
 
-INSERT INTO `#__sdi_sys_server` VALUES ('1','1','1','geoserver' );
-INSERT INTO `#__sdi_sys_server` VALUES ('2','1','1','arcgisserver' );
+INSERT IGNORE INTO `#__sdi_sys_server` VALUES ('1','1','1','geoserver' );
+INSERT IGNORE INTO `#__sdi_sys_server` VALUES ('2','1','1','arcgisserver' );
 
 CREATE TABLE IF NOT EXISTS `#__sdi_sys_server_serviceconnector` (
 `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -19,21 +21,25 @@ CREATE TABLE IF NOT EXISTS `#__sdi_sys_server_serviceconnector` (
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT COLLATE=utf8_general_ci;
 
+CALL drop_foreign_key('sdi_sys_server_serviceconnector', 'sdi_sys_server_serviceconnector_fk1');
 ALTER TABLE `#__sdi_sys_server_serviceconnector`
 ADD CONSTRAINT `#__sdi_sys_server_serviceconnector_fk1` FOREIGN KEY (`server_id`) REFERENCES `#__sdi_sys_server` (`id`);
 
+CALL drop_foreign_key('sdi_sys_server_serviceconnector', 'sdi_sys_server_serviceconnector_fk2');
 ALTER TABLE `#__sdi_sys_server_serviceconnector`
 ADD CONSTRAINT `#__sdi_sys_server_serviceconnector_fk2` FOREIGN KEY (`serviceconnector_id`) REFERENCES `#__sdi_sys_serviceconnector` (`id`);
 
-INSERT INTO `#__sdi_sys_server_serviceconnector` VALUES ('1', '1', '2');
-INSERT INTO `#__sdi_sys_server_serviceconnector` VALUES ('2', '1', '3');
-INSERT INTO `#__sdi_sys_server_serviceconnector` VALUES ('3', '1', '4');
-INSERT INTO `#__sdi_sys_server_serviceconnector` VALUES ('4', '1', '5');
-INSERT INTO `#__sdi_sys_server_serviceconnector` VALUES ('5', '1', '11');
-INSERT INTO `#__sdi_sys_server_serviceconnector` VALUES ('6', '2', '2');
-INSERT INTO `#__sdi_sys_server_serviceconnector` VALUES ('7', '2', '4');
-INSERT INTO `#__sdi_sys_server_serviceconnector` VALUES ('8', '2', '5');
+INSERT IGNORE INTO `#__sdi_sys_server_serviceconnector` VALUES ('1', '1', '2');
+INSERT IGNORE INTO `#__sdi_sys_server_serviceconnector` VALUES ('2', '1', '3');
+INSERT IGNORE INTO `#__sdi_sys_server_serviceconnector` VALUES ('3', '1', '4');
+INSERT IGNORE INTO `#__sdi_sys_server_serviceconnector` VALUES ('4', '1', '5');
+INSERT IGNORE INTO `#__sdi_sys_server_serviceconnector` VALUES ('5', '1', '11');
+INSERT IGNORE INTO `#__sdi_sys_server_serviceconnector` VALUES ('6', '2', '2');
+INSERT IGNORE INTO `#__sdi_sys_server_serviceconnector` VALUES ('7', '2', '4');
+INSERT IGNORE INTO `#__sdi_sys_server_serviceconnector` VALUES ('8', '2', '5');
 
+CALL drop_foreign_key('sdi_physicalservice', 'sdi_physicalservice_server_fk1');
+CALL drop_column('sdi_physicalservice', 'server_id');
 ALTER TABLE `#__sdi_physicalservice` ADD COLUMN `server_id` INT(11) UNSIGNED NULL;
 ALTER TABLE `#__sdi_physicalservice`
 ADD CONSTRAINT `#__sdi_physicalservice_server_fk1` FOREIGN KEY (`server_id`) REFERENCES `#__sdi_sys_server` (`id`);
