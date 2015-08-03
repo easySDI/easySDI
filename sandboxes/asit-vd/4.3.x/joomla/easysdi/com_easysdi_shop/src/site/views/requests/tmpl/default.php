2036c568f774a872be2d23cbdaa0d3468b388cc0
yblatti <yblatti@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-08-03 11:36:35 +0000

    Enhancement #958 (add filters) : Enhance and simplify Orders and Requests views

--- sandboxes/asit-vd/4.3.x/joomla/easysdi/com_easysdi_shop/src/site/views/requests/tmpl/default.php
+++ sandboxes/asit-vd/4.3.x/joomla/easysdi/com_easysdi_shop/src/site/views/requests/tmpl/default.php
@@ -12,8 +12,10 @@ defined('_JEXEC') or die;
 JHtml::_('behavior.keepalive');
 JHTML::_('behavior.modal');
 JHtml::_('behavior.tooltip');
+JHTML::_('behavior.calendar');
 JHtml::_('formbehavior.chosen', 'select');
-
+$document = JFactory::getDocument();
+$document->addScript('components/com_easysdi_shop/views/requests/tmpl/requests.js');
 ?>
 <div class="shop front-end-edit">
     <h1><?php echo JText::_('COM_EASYSDI_SHOP_TITLE_REQUESTS'); ?></h1>
@@ -21,18 +23,54 @@ JHtml::_('formbehavior.chosen', 'select');
         <div class="row-fluid">
             <form class="form-search" action="<?php echo JRoute::_('index.php?option=com_easysdi_shop&view=requests'); ?>" method="post">
                 <div class="btn-group pull-right">
+
+                    <fieldset class="radio btn-group btn-group-yesno" id="filterstatus">
+                        <input type="radio" id="state0" name="filter_status" value="0"<?php if ($this->state->get('filter.status') == 0): ?> checked='checked'<?php endif; ?> onClick="this.form.submit();">
+                        <label for="state0" class="btn"><?php echo JText::_('COM_EASYSDI_SHOP_REQUESTS_STATUS_DONE'); ?></label>
+                        <input type="radio" id="state1" name="filter_status" value="1"<?php if ($this->state->get('filter.status') == 1): ?> checked='checked'<?php endif; ?> onClick="this.form.submit();">
+                        <label for="state1" class="btn"><?php echo JText::_('COM_EASYSDI_SHOP_REQUESTS_STATUS_TODO'); ?></label>
+                    </fieldset>
                     <?php if (count($this->organisms) > 1): ?>
                         <div id="filterorganism" >
                             <select id="filter_userorganism" name="filter_userorganism" onchange="this.form.submit();" class="inputbox">
                                 <option value="" ><?php echo JText::_('COM_EASYSDI_CORE_REQUESTS_ORGANISM_FILTER'); ?></option>
                                 <?php foreach ($this->organisms as $organism): ?>
                                     <option value="<?php echo $organism->id; ?>" <?php
-                            if ($this->state->get('filter.organism') == $organism->id) : echo 'selected="selected"';
-                            endif;
+                                    if ($this->state->get('filter.organism') == $organism->id) : echo 'selected="selected"';
+                                    endif;
                                     ?> ><?php echo $organism->name; ?></option>
                                         <?php endforeach; ?>
                             </select>
                         </div>
+                    <?php endif; ?>
+
+                    
+                    <?php if ($this->state->get('filter.status') == '0'): //advanced filters for "historic" requests (=done)?>
+                        <div id="filterclientorganism" >
+                            <select id="filter_clientorganism" name="filter_clientorganism" onchange="this.form.submit();" class="inputbox">
+                                <option value="" ><?php echo JText::_('COM_EASYSDI_CORE_REQUESTS_CLIENTORGANISM_FILTER'); ?></option>
+                                <?php foreach ($this->clientorganisms as $clientorganism): ?>
+                                    <option value="<?php echo $clientorganism->id; ?>" <?php
+                            if ($this->state->get('filter.clientorganism') == $clientorganism->id) : echo 'selected="selected"';
+                            endif;
+                                    ?> ><?php echo $clientorganism->name; ?></option>
+                                        <?php endforeach; ?>
+                            </select>
+                        </div>
+                    <div id="filtersentfrom" >
+                        <?php 
+                        $datefromval = $this->state->get('filter.sentfrom');
+                        if(!($datefromval & strlen ( $datefromval )>1))
+                            $datefromval = '';
+                        echo JHTML::_('calendar', $datefromval, 'filter_sentfrom', 'filter_sentfrom','%Y-%m-%d',array('placeholder'=>JText::_('COM_EASYSDI_CORE_REQUESTS_DATEFROM_FILTER'),'onchange'=>'this.form.submit();')); ?>
+                    </div>
+                    <div id="filtersentto" >
+                        <?php 
+                        $datetoval = $this->state->get('filter.sentto');
+                        if(!($datetoval & strlen ( $datetoval )>1))
+                            $datetoval = '';
+                        echo JHTML::_('calendar', $datetoval, 'filter_sentto', 'filter_sentto','%Y-%m-%d',array('placeholder'=>JText::_('COM_EASYSDI_CORE_REQUESTS_DATETO_FILTER'),'onchange'=>'this.form.submit();')); ?>
+                    </div>
                     <?php endif; ?>
 
                     <div id="filterstatus">                        
@@ -54,12 +92,7 @@ JHtml::_('formbehavior.chosen', 'select');
                         <button class="btn hasTooltip" type="button" title="Effacer" onclick="document.id('filter_search').value = '';
                                 this.form.submit();"><i class="icon-remove"></i></button>
                     </div>
-
-
-
-
                 </div>
-
             </form>
         </div>
     </div>
