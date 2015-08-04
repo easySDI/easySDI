065e77fb4ac09ad2bcf46659cb71e9e0805de925
yblatti <yblatti@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-08-04 12:58:30 +0000

    Enhancement #896 : Display a message when no results in catalog

--- sandboxes/asit-vd/4.3.x/joomla/easysdi/com_easysdi_catalog/src/site/views/catalog/tmpl/default.php
+++ sandboxes/asit-vd/4.3.x/joomla/easysdi/com_easysdi_catalog/src/site/views/catalog/tmpl/default.php
@@ -18,10 +18,9 @@ $document = JFactory::getDocument();
 $document->addScript('administrator/components/com_easysdi_core/libraries/easysdi/catalog/addToBasket.js');
 $document->addScript('administrator/components/com_easysdi_core/libraries/easysdi/catalog/searchMetadata.js');
 if (JDEBUG) {
-$document->addScript('administrator/components/com_easysdi_core/libraries/openlayers/OpenLayers.debug.js');
-}
-else{
-$document->addScript('administrator/components/com_easysdi_core/libraries/openlayers/OpenLayers.js');
+    $document->addScript('administrator/components/com_easysdi_core/libraries/openlayers/OpenLayers.debug.js');
+} else {
+    $document->addScript('administrator/components/com_easysdi_core/libraries/openlayers/OpenLayers.js');
 }
 ?>
 <style>
@@ -37,52 +36,58 @@ $document->addScript('administrator/components/com_easysdi_core/libraries/openla
 <script type="text/javascript">
     js = jQuery.noConflict();
 
-    js('document').ready(function() {
-        if (!js( '[name="advanced"]' ).length) {
+    js('document').ready(function () {
+        if (!js('[name="advanced"]').length) {
             js("#searchtype").hide();
         }
-        <?php if ($this->isAdvanced()): ?>
+<?php if ($this->isAdvanced()): ?>
             toogleAdvanced();
-        <?php endif; ?>
+<?php endif; ?>
 
-        <?php if ($this->item->oninitrunsearch && JFactory::getApplication()->input->get('search', 'false', 'STRING') == 'false' ): ?>
+<?php if ($this->item->oninitrunsearch && JFactory::getApplication()->input->get('search', 'false', 'STRING') == 'false'): ?>
             //autosubmit search form
             submitForm();
-        <?php endif; ?>
-        <?php if ($this->item->scrolltoresults && JFactory::getApplication()->input->get('search', 'false', 'STRING') == 'true' ): ?>
+<?php endif; ?>
+<?php if ($this->item->scrolltoresults && JFactory::getApplication()->input->get('search', 'false', 'STRING') == 'true'): ?>
             //autoscroll to results
             var sdiSearchResults = jQuery('#sdi-search-results');
-            if(sdiSearchResults.length) jQuery(window).scrollTop(sdiSearchResults.offset().top);
-        <?php endif; ?>
+            if (sdiSearchResults.length)
+                jQuery(window).scrollTop(sdiSearchResults.offset().top);
+<?php endif; ?>
     });
 
 </script>
 
 
 
-<form class="form-horizontal form-validate sdi-catalog-fe-search" action="<?php echo JRoute::_('index.php?option=com_easysdi_catalog' ); ?>#results" method="get" id="searchform" name="searchform" enctype="multipart/form-data">
+<form class="form-horizontal form-validate sdi-catalog-fe-search" action="<?php echo JRoute::_('index.php?option=com_easysdi_catalog'); ?>#results" method="get" id="searchform" name="searchform" enctype="multipart/form-data">
     <?php
     $tmpl = JFactory::getApplication()->input->get('tmpl', null, 'string');
-    if(isset($tmpl)):?>
-    <input type="hidden" name="tmpl" id="tmpl" value="<?php echo $tmpl ; ?>"/>
+    if (isset($tmpl)):
+        ?>
+        <input type="hidden" name="tmpl" id="tmpl" value="<?php echo $tmpl; ?>"/>
     <?php endif; ?>
     <input type="hidden" name="option" id="option" value="com_easysdi_catalog"/>
     <input type="hidden" name="view" id="view" value="catalog"/>
     <input type="hidden" name="search" id="search" value="true"/>
     <input type="hidden" name="id" id="id" value="<?php echo $this->item->id; ?>"/>
-    <input type="hidden" name="preview" id="preview" value="<?php echo $this->preview ; ?>"/>
+    <input type="hidden" name="preview" id="preview" value="<?php echo $this->preview; ?>"/>
     <div class="catalog front-end-edit">
         <fieldset id="searchtype" class="radio btn-group pull-right">
-            <input type="radio" id="jform_searchtype_simple"  name="jform[searchtype]" value="simple" <?php if(!$this->isAdvanced()){ echo 'checked="checked"'; }?>>
-            <label id="lbl_simple" for="jform_searchtype_simple" class="btn searchtype"><?php echo JText::_('COM_EASYSDI_CATALOG_SIMPLE') ; ?></label>
-            <input type="radio" id="jform_searchtype_advanced" name="jform[searchtype]" value="advanced" <?php if($this->isAdvanced()){ echo 'checked="checked"'; }?>>
+            <input type="radio" id="jform_searchtype_simple"  name="jform[searchtype]" value="simple" <?php if (!$this->isAdvanced()) {
+        echo 'checked="checked"';
+    } ?>>
+            <label id="lbl_simple" for="jform_searchtype_simple" class="btn searchtype"><?php echo JText::_('COM_EASYSDI_CATALOG_SIMPLE'); ?></label>
+            <input type="radio" id="jform_searchtype_advanced" name="jform[searchtype]" value="advanced" <?php if ($this->isAdvanced()) {
+        echo 'checked="checked"';
+    } ?>>
             <label id="lbl-advanced" for="jform_searchtype_advanced" class="btn searchtype"><?php echo JText::_('COM_EASYSDI_CATALOG_ADVANCED'); ?></label>
         </fieldset>
         <h1><?php echo JText::_('COM_EASYSDI_CATALOG_TITLE'); ?></h1>
 
         <div class="well">
-            <?php echo $this->getSearchForm(); ?>
-            <button id="btn-submit" class="btn btn-primary" type="submit" ><?php echo JText::_('COM_EASYSDI_CATALOG_SEARCH') ; ?></button>
+<?php echo $this->getSearchForm(); ?>
+            <button id="btn-submit" class="btn btn-primary" type="submit" ><?php echo JText::_('COM_EASYSDI_CATALOG_SEARCH'); ?></button>
         </div>
     </div>
 </form>
@@ -94,22 +99,25 @@ if ($results):
     <div class="catalog-searchresults" id="sdi-search-results">
         <h3><?php echo JFactory::getApplication('com_easysdi_catalog')->getUserState('global.list.total') . ' ' . JText::_("COM_EASYSDI_CATALOG_RESULTS_TITLE"); ?></h3>
         <?php
-        // Build of extendend XML for each result entry
-        foreach ($results as $result) :
-            ?><div class="offset1 catalog-searchresult"> <?php
-            echo $result;
-            ?></div>
+            // Build of extendend XML for each result entry
+            foreach ($results as $result) :
+                ?><div class="offset1 catalog-searchresult"> <?php
+                echo $result;
+                ?></div>
             <hr><?php endforeach; ?>
 
     </div>
     <div class="pagination">
         <p class="counter">
-            <?php echo $this->pagination->getPagesCounter(); ?>
+    <?php echo $this->pagination->getPagesCounter(); ?>
         </p>
-        <?php echo $this->pagination->getPagesLinks(); ?>
+    <?php echo $this->pagination->getPagesLinks(); ?>
     </div>
 
     <?php
+else:
+    //empty results string, or article if provided
+    echo $this->getEmptyResultContent(EText::_($this->item->guid,2,''));
 endif;
 ?>
 
