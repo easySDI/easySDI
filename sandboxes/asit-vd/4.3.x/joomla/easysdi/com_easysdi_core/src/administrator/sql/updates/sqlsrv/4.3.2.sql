0bfe467090df3c2e73fe4ea426316c9cff677369
yblatti <yblatti@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-09-16 14:38:38 +0000

    Defect #1132 : Pricing : total DB type needs to be DECIMAL(19,2) as for config values

--- sandboxes/asit-vd/4.3.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.3.2.sql
+++ sandboxes/asit-vd/4.3.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.3.2.sql
@@ -87,3 +87,14 @@ ALTER TABLE [#__sdi_order] ALTER COLUMN [remark] NVARCHAR (4000);
 
 UPDATE [#__sdi_sys_pricing] SET [value] = 'feewithoutapricingprofile' WHERE [id] = 2;
 UPDATE [#__sdi_sys_pricing] SET [value] = 'feewithapricingprofile' WHERE [id] = 3;
+
+ALTER TABLE [#__sdi_pricing_order_supplier_product] ALTER COLUMN [cal_amount_data_te]   decimal(19,2) NULL;
+ALTER TABLE [#__sdi_pricing_order_supplier_product] ALTER COLUMN [cal_total_amount_te]  decimal(19,2) NULL;
+ALTER TABLE [#__sdi_pricing_order_supplier_product] ALTER COLUMN [cal_total_amount_ti]  decimal(19,2) NULL;
+ALTER TABLE [#__sdi_pricing_order_supplier_product] ALTER COLUMN [cal_total_rebate_ti]  decimal(19,2) NOT NULL;
+ALTER TABLE [#__sdi_pricing_order_supplier_product] ADD CONSTRAINT DF_cal_total_rebate_ti DEFAULT 0 FOR [cal_total_rebate_ti];
+
+ALTER TABLE [#__sdi_pricing_order_supplier] ALTER COLUMN [cal_total_rebate_ti]  decimal(19,2) NOT NULL;
+ALTER TABLE [#__sdi_pricing_order_supplier] ALTER COLUMN [cal_total_amount_ti]  decimal(19,2) NULL;
+
+ALTER TABLE [#__sdi_pricing_order] ALTER COLUMN [cal_total_amount_ti]  decimal(19,2) NULL;
