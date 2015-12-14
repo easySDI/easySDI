dd7c26a89bef39c3b2632bcc5f8da8d197b1d7e7
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2015-12-14 13:32:59 +0000

    [yblatti] Enhancement #1214 : Move order id in the recaps in the H1 element

--- branches/4.4.x/joomla/easysdi/com_easysdi_shop/src/site/views/request/tmpl/default.php
+++ branches/4.4.x/joomla/easysdi/com_easysdi_shop/src/site/views/request/tmpl/default.php
@@ -9,6 +9,9 @@
 // no direct access
 defined('_JEXEC') or die;
 
+JHtml::_('behavior.keepalive');
+JHtml::_('behavior.tooltip');
+
 require_once JPATH_SITE . '/components/com_easysdi_shop/helpers/easysdi_shop.php';
 
 $document = JFactory::getDocument();
@@ -34,7 +37,7 @@ if ($this->item) :
     Easysdi_shopHelper::basketReloadSavedPricing($this->item->basket);
     ?>
 
-    <h1><?php echo JText::_('COM_EASYSDI_SHOP_ORDER_TITLE'); ?></h1>
+    <h1><?php echo JText::_('COM_EASYSDI_SHOP_REQUEST_TITLE'); ?> <span id="sdi-order-title-id"><?php echo $this->item->id; ?></span></h1>
 
     <div class="order-edit front-end-edit">
         <form class="form-inline form-validate" action="<?php echo JRoute::_('index.php?option=com_easysdi_shop&view=request'); ?>" method="post" id="adminForm" name="adminForm" enctype="multipart/form-data">
