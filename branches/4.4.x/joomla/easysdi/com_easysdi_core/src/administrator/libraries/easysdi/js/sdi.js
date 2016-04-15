e6d622864beb3ba9592138f91147724c677b7029
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2016-04-15 15:55:05 +0000

    [tportier] #1306 : Can't assign specific rules to admin map management #1305 : Change leaflet sidebar css class #1304 : google geocoder doesn't work anymore when searching by lat/lon #1303 : mail not sent when using wps setorder request #1293 : Can't remove processing #1291 : Can't manage admin rules for component processing

--- branches/4.4.x/joomla/easysdi/com_easysdi_core/src/administrator/libraries/easysdi/js/sdi.js
+++ branches/4.4.x/joomla/easysdi/com_easysdi_core/src/administrator/libraries/easysdi/js/sdi.js
@@ -974,6 +974,106 @@ sdi.gxp.plugins.WMSSource = Ext.extend(gxp.plugins.WMSSource, {
 Ext.preg(sdi.gxp.plugins.WMSSource.prototype.ptype, sdi.gxp.plugins.WMSSource);
 
 /**
+ * Copyright (c) 2008-2011 The Open Planning Project
+ * 
+ * Published under the GPL license.
+ * See https://github.com/opengeo/gxp/raw/master/license.txt for the full text
+ * of the license.
+ */
+
+/**
+ * @requires plugins/Tool.js
+ * @requires widgets/form/GoogleGeocoderComboBox.js
+ */
+
+/** api: (define)
+ *  module = gxp.plugins
+ *  class = GoogleGeocoder
+ */
+
+/** api: (extends)
+ *  plugins/Tool.js
+ */
+Ext.namespace("sdi.gxp.plugins");
+
+/** api: constructor
+ *  .. class:: GoogleGeocoder(config)
+ *
+ *    Plugin for adding a GoogleGeocoderComboBox to a viewer.  The underlying
+ *    GoogleGeocoderComboBox can be configured by setting this tool's 
+ *    ``outputConfig`` property. The gxp.form.GoogleGeocoderComboBox requires 
+ *    the gxp.plugins.GoogleSource or the Google Maps V3 API to be loaded.
+ */
+sdi.gxp.plugins.GoogleGeocoder = Ext.extend(gxp.plugins.Tool, {
+    
+    /** api: ptype = gxp_googlegeocoder */
+    ptype: "sdi_gxp_googlegeocoder",
+
+    /** api: config[updateField]
+     *  ``String``
+     *  If value is specified, when an item is selected in the combo, the map
+     *  will be zoomed to the corresponding field value in the selected record.
+     *  If ``null``, no map navigation will occur.  Valid values are the field
+     *  names described for the :class:`gxp.form.GoogleGeocoderComboBox`.
+     *  Default is "viewport".
+     */
+    updateField: "viewport",
+    
+    init: function(target) {
+
+        var combo = new sdi.gxp.form.GoogleGeocoderComboBox(Ext.apply({
+            listeners: {
+                select: this.onComboSelect,
+                scope: this
+            }
+        }, this.outputConfig));
+        
+        var bounds = target.mapPanel.map.restrictedExtent;
+        if (bounds && !combo.bounds) {
+            target.on({
+                ready: function() {
+                    combo.bounds = bounds.clone().transform(
+                        target.mapPanel.map.getProjectionObject(),
+                        new OpenLayers.Projection("EPSG:4326")
+                    );
+                }
+            });
+        }
+        this.combo = combo;
+        
+        return sdi.gxp.plugins.GoogleGeocoder.superclass.init.apply(this, arguments);
+
+    },
+    
+    /** api: method[addOutput]
+     */
+    addOutput: function(config) {
+        return sdi.gxp.plugins.GoogleGeocoder.superclass.addOutput.call(this, this.combo);
+    },
+    
+    /** private: method[onComboSelect]
+     *  Listener for combo's select event.
+     */
+    onComboSelect: function(combo, record) {
+        if (this.updateField) {
+            var map = this.target.mapPanel.map;
+            var location = record.get(this.updateField).clone().transform(
+                new OpenLayers.Projection("EPSG:4326"),
+                map.getProjectionObject()
+            );
+            if (location instanceof OpenLayers.Bounds) {
+                map.zoomToExtent(location, true);
+            } else {
+                map.setCenter(location);
+            }
+        }
+    }
+
+});
+
+Ext.preg(sdi.gxp.plugins.GoogleGeocoder.prototype.ptype, sdi.gxp.plugins.GoogleGeocoder);
+
+/**
  * @version     4.4.0
  * @package     com_easysdi_core
  * @copyright   Copyright (C) 2013-2016. All rights reserved.
@@ -1187,6 +1287,157 @@ gxp.Viewer.prototype.initMapPanel = function () {
 
 
 
+
+
+/**
+ * @version     4.4.0
+ * @package     com_easysdi_core
+ * @copyright   Copyright (C) 2013-2016. All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE.txt
+ * @author      EasySDI Community <contact@easysdi.org> - http://www.easysdi.org
+ */
+Ext.namespace("sdi.gxp.form");
+
+sdi.gxp.form.GoogleGeocoderComboBox = Ext.extend(gxp.form.GoogleGeocoderComboBox, {
+    
+    /** api: xtype = gxp_googlegeocodercombo */
+    xtype: "sdi_gxp_googlegeocodercombo",
+
+    /** api: config[queryDelay]
+     *  ``Number`` Delay before the search occurs.  Default is 100ms.
+     */
+    queryDelay: 100,
+    
+    /** api: config[bounds]
+     *  ``OpenLayers.Bounds | Array`` Optional bounds (in geographic coordinates)
+     *  for restricting search.
+     */
+    
+    /** api: config[valueField]
+     *  ``String``
+     *  Field from selected record to use when the combo's ``getValue`` method
+     *  is called.  Default is "location".  Possible value's are "location",
+     *  "viewport", or "address".  The location value will be an 
+     * ``OpenLayers.LonLat`` object that corresponds to the geocoded address.
+     *  The viewport value will be an ``OpenLayers.Bounds`` object that is 
+     *  the recommended viewport for viewing the resulting location.  The
+     *  address value will be a string that is the formatted address.
+     */
+    valueField: "viewport",
+
+    /** private: config[displayField]
+     */
+    displayField: "address",
+    
+    /** private: method[initComponent]
+     *  Override
+     */
+    initComponent: function() {
+        
+        // only enable when Google Maps API is available
+        this.disabled = true;
+        var ready = !!(window.google && google.maps);
+        if (!ready) {
+            if (!gxp.plugins || !gxp.plugins.GoogleSource) {
+                throw new Error("The gxp.form.GoogleGeocoderComboBox requires the gxp.plugins.GoogleSource or the Google Maps V3 API to be loaded.");
+            }
+            gxp.plugins.GoogleSource.loader.onLoad({
+                otherParams: gxp.plugins.GoogleSource.prototype.otherParams,
+                callback: this.prepGeocoder,
+                errback: function() {
+                    throw new Error("The Google Maps script failed to load within the given timeout.");
+                },
+                scope: this
+            });
+        } else {
+            // call in the next turn to complete initialization
+            window.setTimeout((function() {
+                this.prepGeocoder();
+            }).createDelegate(this), 0);
+        }
+
+        this.store = new Ext.data.JsonStore({
+            root: "results",
+            fields: [
+                {name: "address", type: "string"},
+                {name: "location"}, // OpenLayers.LonLat
+                {name: "viewport"} // OpenLayers.Bounds
+            ],
+            autoLoad: false
+        });
+        
+        this.on({
+            focus: function() {
+                this.clearValue();
+            },
+            scope: this
+        });
+        
+        return sdi.gxp.form.GoogleGeocoderComboBox.superclass.initComponent.apply(this, arguments);
+
+    },
+    
+    prepGeocoder: function() {
+        var geocoder = new google.maps.Geocoder();
+        
+
+        // create an async proxy for getting geocoder results
+        var api = {};
+        api[Ext.data.Api.actions.read] = true;
+        var proxy = new Ext.data.DataProxy({api: api});
+        var combo = this;
+        
+        // TODO: unhack this - this is due to the the tool output being generated too early
+        var getBounds = (function() {
+            // optional bounds for restricting search
+            var bounds = this.bounds;
+            if (bounds) {
+                if (bounds instanceof OpenLayers.Bounds) {
+                    bounds = bounds.toArray();
+                }
+                bounds = new google.maps.LatLngBounds(
+                    new google.maps.LatLng(bounds[1], bounds[0]),
+                    new google.maps.LatLng(bounds[3], bounds[2])
+                );
+            }
+            return bounds;
+        }).createDelegate(this);
+        
+        proxy.doRequest = function(action, rs, params, reader, callback, scope, options) {
+            // Assumes all actions read.
+            geocoder.geocode(
+                {address: params.query/*, bounds: getBounds()*/},
+                function(results, status) {
+                    var readerResult;
+                    if (status === google.maps.GeocoderStatus.OK || 
+                        status === google.maps.GeocoderStatus.ZERO_RESULTS) {
+                        try {
+                            results = combo.transformResults(results);
+                            readerResult = reader.readRecords({results: results});
+                        } catch (err) {
+                            combo.fireEvent("exception", combo, "response", action, options, status, err);
+                        }
+                    } else {
+                        combo.fireEvent("exception", combo, "remote", action, options, status, null);
+                    }
+                    if (readerResult) {
+                        callback.call(scope, readerResult, options, true);                        
+                    } else {
+                        callback.call(scope, null, options, false);                        
+                    }
+                }
+            );
+        };
+        
+        this.store.proxy = proxy;
+        if (this.initialConfig.disabled != true) {
+            this.enable();
+        }
+    }
+    
+    });
+
+Ext.reg('sdi_gxp_googlegeocodercombobox', sdi.gxp.form.GoogleGeocoderComboBox);
 
 
 /**
