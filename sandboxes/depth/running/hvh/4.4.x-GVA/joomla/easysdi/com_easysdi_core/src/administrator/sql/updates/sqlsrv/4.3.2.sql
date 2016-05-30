dab7b36d86f2cc99567061f5254b9e996247471b
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2016-05-30 16:00:31 +0000

    Several SQL statements corrections to be compliant with SQLServer

--- sandboxes/depth/running/hvh/4.4.x-GVA/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.3.2.sql
+++ sandboxes/depth/running/hvh/4.4.x-GVA/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.3.2.sql
@@ -26,44 +26,28 @@ ALTER TABLE [#__sdi_pricing_profile] ALTER COLUMN [fixed_fee]  decimal(19,2) NUL
 ALTER TABLE [#__sdi_pricing_profile]  ALTER COLUMN [surface_rate]  decimal(19,2) NULL;
 ALTER TABLE [#__sdi_pricing_profile] ALTER COLUMN [min_fee]  decimal(19,2) NULL ;
 ALTER TABLE [#__sdi_pricing_profile]  ALTER COLUMN [max_fee]  decimal(19,2) NULL;
-
 ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ALTER COLUMN [cfg_fixed_fee]  decimal(19,2) NOT NULL;
-ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ADD CONSTRAINT DF_cfg_fixed_fee DEFAULT 0.00 FOR [cfg_fixed_fee];
 ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ALTER COLUMN [cfg_surface_rate]  decimal(19,2) NOT NULL;
-ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ADD CONSTRAINT DF_cfg_surface_rate DEFAULT 0.00 FOR [cfg_surface_rate];
 ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ALTER COLUMN [cfg_min_fee]  decimal(19,2) NOT NULL ;
-ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ADD CONSTRAINT DF_cfg_min_fee DEFAULT 0.00 FOR [cfg_min_fee];
 ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ALTER COLUMN [cfg_max_fee]  decimal(19,2) NOT NULL ;
-ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ADD CONSTRAINT DF_cfg_max_fee DEFAULT 0.00 FOR [cfg_max_fee];
 ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ALTER COLUMN [cfg_pct_category_profile_discount]  decimal(19,2) NOT NULL ;
-ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ADD CONSTRAINT DF_cfg_pct_cpd DEFAULT 0.00 FOR [cfg_pct_category_profile_discount];
-
 ALTER TABLE [#__sdi_pricing_order_supplier_product] ALTER COLUMN [cfg_pct_category_supplier_discount]  decimal(19,2) NOT NULL;
-ALTER TABLE [#__sdi_pricing_order_supplier_product] ADD CONSTRAINT DF_cfg_pct_csd DEFAULT 0.00 FOR [cfg_pct_category_supplier_discount];
-
 ALTER TABLE [#__sdi_pricing_order_supplier] ALTER COLUMN [cfg_fixed_fee_ti]  decimal(19,2) NOT NULL;
-ALTER TABLE [#__sdi_pricing_order_supplier] ADD CONSTRAINT DF_cfg_fixed_fee_ti DEFAULT 0.00 FOR [cfg_fixed_fee_ti];
 ALTER TABLE [#__sdi_pricing_order_supplier] ALTER COLUMN [cal_fee_ti]  decimal(19,2) NOT NULL;
-ALTER TABLE [#__sdi_pricing_order_supplier] ADD CONSTRAINT DF_cal_fee_ti DEFAULT 0.00 FOR [cal_fee_ti];
-
 ALTER TABLE [#__sdi_pricing_order] ALTER COLUMN [cfg_vat]  decimal(19,2) NOT NULL;
-ALTER TABLE [#__sdi_pricing_order] ADD CONSTRAINT DF_cfg_vat DEFAULT 0.00 FOR [cfg_vat];
 ALTER TABLE [#__sdi_pricing_order] ALTER COLUMN [cfg_overall_default_fee]  decimal(19,2) NOT NULL;
-ALTER TABLE [#__sdi_pricing_order] ADD CONSTRAINT DF_cfg_overall_default_fee DEFAULT 0.00 FOR [cfg_overall_default_fee];
 ALTER TABLE [#__sdi_pricing_order] ALTER COLUMN [cal_fee_ti]  decimal(19,2) NOT NULL;
-ALTER TABLE [#__sdi_pricing_order] ADD CONSTRAINT DF_cal_fee_ti_po DEFAULT 0.00 FOR [cal_fee_ti];
-
 ALTER TABLE [#__sdi_organism_category_pricing_rebate] ALTER COLUMN [rebate]  decimal(19,2) NULL;
-
 ALTER TABLE [#__sdi_pricing_profile] ADD CONSTRAINT DF_pp_state DEFAULT 1 FOR [state];
 ALTER TABLE [#__sdi_pricing_profile] ADD CONSTRAINT DF_pp_checkedout  DEFAULT '0'  FOR [checked_out];
 ALTER TABLE [#__sdi_pricing_profile] ADD CONSTRAINT DF_pp_checkedouttime DEFAULT '1900-01-01T00:00:00.000' FOR [checked_out_time];
 
 ALTER TABLE [#__sdi_diffusion] ADD [packageurl] [nvarchar](500);
-UPDATE [#__sdi_diffusion] SET [packageurl]='{CODE}';
+UPDATE [#__sdi_diffusion] SET [packageurl] ='{CODE}' WHERE [id] IS NOT NULL;
 
 CREATE NONCLUSTERED INDEX IX_NC_text1 ON [#__sdi_translation] (text1);
 CREATE NONCLUSTERED INDEX IX_NC_text2 ON [#__sdi_translation] (text2);
+ALTER TABLE  [#__sdi_order] DROP CONSTRAINT DF__sdi_sdi_o__valid__2F650636;
 ALTER TABLE [#__sdi_order] DROP COLUMN [validate];
 ALTER TABLE [#__sdi_order] ADD [validated] [smallint];
 
@@ -111,7 +95,7 @@ UPDATE [#__sdi_sys_orderstate] SET [ordering] = 8  WHERE [id] = 2;
 UPDATE [#__sdi_sys_orderstate] SET [ordering] = 9  WHERE [id] = 9;
 UPDATE [#__sdi_sys_orderstate] SET [ordering] = 10 WHERE [id] = 10;
 
-ALTER TABLE [#__sdi_order] ADD  [validated_by] [int] NULL;
+ALTER TABLE [#__sdi_order] ADD  [validated_by] [bigint] NULL;
 ALTER TABLE #__sdi_order ADD CONSTRAINT #__sdi_order$#__sdi_user_fk5
 FOREIGN KEY ([validated_by]) REFERENCES [#__sdi_user] ([id]) ON DELETE CASCADE ON UPDATE NO ACTION;
 
