94f3c4b91c8d2dcc12bc2b47e3607ca82c64ac75
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2016-05-30 15:50:01 +0000

    fix SQL Server statements for 4.4.1

--- sandboxes/depth/running/hvh/4.4.x-GVA/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.4.1.sql
+++ sandboxes/depth/running/hvh/4.4.x-GVA/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.4.1.sql
@@ -1,4 +1,3 @@
-ALTER TABLE [#__sdi_diffusion_download] DROP CONSTRAINT [#__sdi_diffusion_download$#__sdi_diffusion_download_fk2];
 ALTER TABLE [#__sdi_diffusion_download]  WITH NOCHECK ADD  CONSTRAINT [#__sdi_diffusion_download$#__sdi_diffusion_download_fk2] FOREIGN KEY([user_id])
 REFERENCES [#__sdi_user] ([id])
 ON DELETE SET NULL;
@@ -6,7 +5,7 @@ ON DELETE SET NULL;
 ALTER TABLE [#__sdi_order] ADD [archived] [int] NOT NULL DEFAULT 0;
 UPDATE [#__sdi_order] SET [archived]=1 WHERE [orderstate_id]=1;
 UPDATE [#__sdi_order] SET [orderstate_id]=3 WHERE [orderstate_id]=1;
-DELETE FROM [#__sdi_sys_orderstate] WHERE `id`=1;
+DELETE FROM [#__sdi_sys_orderstate] WHERE [id]=1;
 
 ALTER TABLE  [#__sdi_order_diffusion] DROP COLUMN [fee];
 
