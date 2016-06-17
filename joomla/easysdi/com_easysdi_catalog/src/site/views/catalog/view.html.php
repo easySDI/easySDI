997fadce61040e753b9db6e015add1bd34feba4c
Hélène Van Hoecke <2844416+hvanhoecke@users.noreply.github.com>
2016-06-17 08:54:49 +0000

    [yblatti] Headers : version 4.4.1

--- joomla/easysdi/com_easysdi_catalog/src/site/views/catalog/view.html.php
+++ joomla/easysdi/com_easysdi_catalog/src/site/views/catalog/view.html.php
@@ -3,7 +3,7 @@
 require_once JPATH_BASE . '/components/com_easysdi_catalog/libraries/easysdi/SearchHtmlForm.php';
 
 /**
- * @version     4.4.0
+ * @version     4.4.1
  * @package     com_easysdi_catalog
  * @copyright   Copyright (C) 2013-2016. All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
