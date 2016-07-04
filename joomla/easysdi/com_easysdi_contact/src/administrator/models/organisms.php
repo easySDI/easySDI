b9a84a1530103a1223f1af7478debe40bb6b0d3c
Hélène Van Hoecke <2844416+hvanhoecke@users.noreply.github.com>
2016-07-04 08:51:56 +0000

    [yblatti] Files headers for 4.4.2

--- joomla/easysdi/com_easysdi_contact/src/administrator/models/organisms.php
+++ joomla/easysdi/com_easysdi_contact/src/administrator/models/organisms.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @version     4.4.1
+ * @version     4.4.2
  * @package     com_easysdi_contact
  * @copyright   Copyright (C) 2013-2016. All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
