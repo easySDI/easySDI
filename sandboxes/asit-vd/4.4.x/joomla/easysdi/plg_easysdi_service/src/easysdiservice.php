4ab948c3bc91f43522662e77b07a0bcbd8ecc81a
yblatti <yblatti@f71f33a3-9987-4f9c-9609-a93d63a967da>
2016-08-09 08:13:17 +0000

    Files headers for 4.4.3

--- sandboxes/asit-vd/4.4.x/joomla/easysdi/plg_easysdi_service/src/easysdiservice.php
+++ sandboxes/asit-vd/4.4.x/joomla/easysdi/plg_easysdi_service/src/easysdiservice.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @version     4.4.2
+ * @version     4.4.3
  * @package     plg_easysdi_service
  * @copyright   Copyright (C) 2013-2016. All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
