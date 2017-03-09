3cf49d37d58a745e8e2fcd7c664e4b77ce050eb1
Hélène Van Hoecke <2844416+hvanhoecke@users.noreply.github.com>
2017-03-09 07:42:51 +0000

    [yblatti] Version 4.4.4 in POM files and files headers

--- joomla/easysdi/com_easysdi_shop/src/site/controllers/extract.php
+++ joomla/easysdi/com_easysdi_shop/src/site/controllers/extract.php
@@ -4,9 +4,9 @@
  * Extract - WebService designed to getOrders and setProduct
  * based on the old webservice (rest)
  * 
- * @version     4.4.3
+ * @version     4.4.4
  * @package     com_easysdi_shop
- * @copyright   Copyright (C) 2013-2016. All rights reserved.
+ * @copyright   Copyright (C) 2013-2017. All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
  * @author      EasySDI Community <contact@easysdi.org> - http://www.easysdi.org
  */
