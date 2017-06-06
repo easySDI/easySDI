9b87d8171b9b56d219a65ecc9a776d77142484ef
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2017-06-06 14:40:11 +0000

    [yblatti] Bump version to 4.4.5

--- branches/4.4.x/joomla/easysdi/plg_easysdi_contact/src/easysdicontact.php
+++ branches/4.4.x/joomla/easysdi/plg_easysdi_contact/src/easysdicontact.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* @version     4.4.4
+* @version     4.4.5
 * @package     plg_easysdi_user
 * @copyright   Copyright (C) 2013-2017. All rights reserved.
 * @license     GNU General Public License version 3 or later; see LICENSE.txt
