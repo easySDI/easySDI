52d0057e831c23a321ef2fcba0cec99243a2573d
mbattaglia <mbattaglia@f71f33a3-9987-4f9c-9609-a93d63a967da>
2017-06-09 09:22:10 +0000

    Delete

--- sandboxes/depth/running/mb/4.4.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.3.0-beta-2.sql
+++
@@ -1,2 +0,0 @@
-CALL drop_column('sdi_catalog', 'scrolltoresults');
-ALTER TABLE `#__sdi_catalog` ADD `scrolltoresults` TINYINT(1) NOT NULL DEFAULT '1';
--- sandboxes/depth/running/mb/4.4.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.4.5.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `#__sdi_diffusion` ADD COLUMN `otp` tinyint(1) NOT NULL DEFAULT 0 AFTER `restrictedperimeter`;
-ALTER TABLE `#__sdi_order_diffusion` ADD COLUMN `otp` TEXT NULL AFTER `displayName`;
-ALTER TABLE `#__sdi_order_diffusion` ADD COLUMN `otpchance` INT(11) DEFAULT 0 AFTER `otp`;
-INSERT IGNORE INTO `#__sdi_sys_productstate` VALUES ('8', '8', '1', 'blocked');
