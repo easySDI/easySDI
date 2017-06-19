a160809d84f3a392435fa897a91037b537a426e2
yblatti <yblatti@f71f33a3-9987-4f9c-9609-a93d63a967da>
2017-06-19 09:05:42 +0000

    Merged revision(s) 10201 from branches/4.4.x: Merged revision(s) 10199-10200 from sandboxes/depth/running/mb/4.4.x: Défaut #16661 point 3 Defect #1424

--- sandboxes/asit-vd/4.4.x/joomla/easysdi/com_easysdi_contact/src/administrator/models/users.php
+++ sandboxes/asit-vd/4.4.x/joomla/easysdi/com_easysdi_contact/src/administrator/models/users.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @version     4.4.5
+ * @version     4.4.4
  * @package     com_easysdi_contact
  * @copyright   Copyright (C) 2013-2017. All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
@@ -116,7 +116,7 @@ class Easysdi_contactModelusers extends JModelList {
      * @since	1.6
      */
     protected function getListQuery() {
-
+        
         // Create a new query object.
         $db = $this->getDbo();
         $query = $db->getQuery(true);
@@ -125,7 +125,8 @@ class Easysdi_contactModelusers extends JModelList {
         // Select the required fields from the table.
         $query->select(
                 $this->getState(
-                        'list.select', 'a.id, a.checked_out, a.checked_out_time, a.created_by, a.ordering,a.state,a.catid,' . $query->concatenate(array($db->qn('o.name'), "' ['", $db->qn('o.id'), "']'")) . ' AS member_organism, o.id as member_organism_id'
+                        //'list.select', 'a.id, a.checked_out, a.checked_out_time, a.created_by, a.ordering,a.state,a.catid,' . $query->concatenate(array($db->qn('o.name'), "' ['", $db->qn('o.guid'), "']'")) . ' AS member_organism, o.id as member_organism_id'
+                        'list.select', 'a.id, a.checked_out, a.checked_out_time, a.created_by, a.ordering,a.state,a.catid,CONCAT(o.name,\' [\',o.id,\']\') AS member_organism, o.id as member_organism_id'
                 )
         );
         $query->from('#__sdi_user AS a');
@@ -177,10 +178,7 @@ class Easysdi_contactModelusers extends JModelList {
         } elseif ($published === '') {
             $query->where('(a.state = 0 OR a.state = 1)');
         }
-
-
-
-
+        
         // Filter by organism
         $organisms = $this->getState('filter.organism');
         if (count($organisms)) {
@@ -207,7 +205,7 @@ class Easysdi_contactModelusers extends JModelList {
         //Not necessary
         // group by user.id to have unique rows in result
         //$query->group('a.id');
-
+        
         return $query;
     }
 
