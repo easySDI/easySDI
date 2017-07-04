0e0632ffd53f10d4974613e83fd4aa97bd52191f
tportier <tportier@f71f33a3-9987-4f9c-9609-a93d63a967da>
2017-07-04 13:15:59 +0000

    Merged revision(s) 10222-10233 from branches/4.4.x: Auto Commit from continuous integration. (easySDI_4.4.x:577 http://localhost:8080/jenkins/job/easySDI_4.4.x/577/) ........ Auto Commit from continuous integration. (easySDI_4.4.x:578 http://localhost:8080/jenkins/job/easySDI_4.4.x/578/) ........ Réalisation #16875 ........ [tportier] #1425 : defaultvalue of sdi_catalog_searchcriteria too small
    ........

--- sandboxes/depth.fr/tp/4.4.5/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.4.5.sql
+++ sandboxes/depth.fr/tp/4.4.5/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.4.5.sql
@@ -3,6 +3,5 @@ ALTER TABLE `#__sdi_order_diffusion` ADD COLUMN `otp` TEXT NULL AFTER `displayNa
 ALTER TABLE `#__sdi_order_diffusion` ADD COLUMN `otpchance` INT(11) DEFAULT 0 AFTER `otp`;
 ALTER TABLE `#__sdi_catalog_searchcriteria` MODIFY COLUMN `defaultvalue` text;
 CALL drop_foreign_key('sdi_pricing_order_supplier_product_profile', 'sdi_pricing_order_supplier_product_profile_fk2');
-ALTER TABLE `#__sdi_pricing_order_supplier_product_profile`
-MODIFY `pricing_profile_id` INT(11) UNSIGNED NULL,
+ALTER TABLE `#__sdi_pricing_order_supplier_product_profile` MODIFY `pricing_profile_id` INT(11) UNSIGNED NULL, 
 ADD CONSTRAINT `#__sdi_pricing_order_supplier_product_profile_fk2` FOREIGN KEY (`pricing_profile_id`) REFERENCES `#__sdi_pricing_profile` (`id`) ON UPDATE NO ACTION ON DELETE SET NULL;
