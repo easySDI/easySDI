99e154825ff3227aa1f109814b5001445d4dd22c
yblatti <yblatti@f71f33a3-9987-4f9c-9609-a93d63a967da>
2018-02-22 17:20:13 +0000

    [yblatti] Defect #1448 : Update headers for version and year in V4.4.5

--- sandboxes/asit-vd/4.4.x/joomla/easysdi/mod_easysdi_adminbutton/src/tmpl/default.php
+++ sandboxes/asit-vd/4.4.x/joomla/easysdi/mod_easysdi_adminbutton/src/tmpl/default.php
@@ -2,7 +2,7 @@
 /**
  * @version     4.4.5
  * @package     mod_easysdi_adminbutton
- * @copyright   Copyright (C) 2013-2017. All rights reserved.
+ * @copyright   Copyright (C) 2013-2018. All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
  * @author      EasySDI Community <contact@easysdi.org> - http://www.easysdi.org
  */
