5bea75cdf7831b2d8274d2d452a6c4cd1e5ca132
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2018-04-09 15:58:53 +0000

    [yblatti] FIX SQL update for Enhancement #1443 : Add the ability to select default background for a map context

--- branches/4.5.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.4.5.sql
+++ branches/4.5.x/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/mysql/4.4.5.sql
@@ -6,4 +6,4 @@ ALTER TABLE `#__sdi_catalog_searchcriteria` MODIFY COLUMN `defaultvalue` text;
 CALL drop_foreign_key('sdi_pricing_order_supplier_product_profile', 'sdi_pricing_order_supplier_product_profile_fk2');
 ALTER TABLE `#__sdi_pricing_order_supplier_product_profile` MODIFY `pricing_profile_id` INT(11) UNSIGNED NULL, 
 ADD CONSTRAINT `#__sdi_pricing_order_supplier_product_profile_fk2` FOREIGN KEY (`pricing_profile_id`) REFERENCES `#__sdi_pricing_profile` (`id`) ON UPDATE NO ACTION ON DELETE SET NULL;
-ALTER TABLE `#__sdi_map` ADD COLUMN `default_backgroud_layer` NOT NULL DEFAULT '0' AFTER `state`;
+ALTER TABLE `#__sdi_map` ADD COLUMN `default_backgroud_layer` INT(11) NOT NULL DEFAULT 0 AFTER `state`;
