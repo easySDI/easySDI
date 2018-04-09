51a2dced98b8514cad56194d99890ba81342237b
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2018-04-09 06:45:35 +0000

    Evolution #18133 : Adaptation mod_languages

--- sandboxes/depth/running/hvh/4.3.16/joomla/easysdi/com_easysdi_catalog/src/script.php
+++ sandboxes/depth/running/hvh/4.3.16/joomla/easysdi/com_easysdi_catalog/src/script.php
@@ -111,7 +111,7 @@ class com_easysdi_catalogInstallerScript {
         }
         
         if ($jversion->getShortVersion() >= '3.3.16') {
-            copy(JPATH_ROOT . '/administrator/components/com_easysdi_catalog/assets/patch/module/mod_languages/helper.php', JPATH_ROOT . '/module/mod_languages/helper.php');
+            copy(JPATH_ROOT . '/administrator/components/com_easysdi_catalog/assets/patch/modules/mod_languages/helper.php', JPATH_ROOT . '/modules/mod_languages/helper.php');
         }
 
         $db = JFactory::getDbo();
