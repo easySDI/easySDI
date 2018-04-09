90dd43c4465a0ce4501de983e4ec9bb55c6e2d39
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2018-04-09 16:08:03 +0000

    [yblatti] Defect #1448 : Update headers for version and year in V4.4.5

--- branches/4.5.x/joomla/easysdi/mod_easysdi_lastorders/src/mod_easysdi_lastorders.php
+++ branches/4.5.x/joomla/easysdi/mod_easysdi_lastorders/src/mod_easysdi_lastorders.php
@@ -3,7 +3,7 @@
 /**
  * @version     4.4.5
  * @package     mod_easysdi_lastorders
- * @copyright   Copyright (C) 2013-2017. All rights reserved.
+ * @copyright   Copyright (C) 2013-2018. All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
  * @author      EasySDI Community <contact@easysdi.org> - http://www.easysdi.org
  */
