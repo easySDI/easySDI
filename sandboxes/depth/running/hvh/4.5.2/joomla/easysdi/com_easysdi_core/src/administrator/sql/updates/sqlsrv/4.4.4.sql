b4d665a534639fc74a30fea0188f28ab0ccb257d
hvanhoecke <hvanhoecke@f71f33a3-9987-4f9c-9609-a93d63a967da>
2019-06-27 14:49:15 +0000

    Syntaxe du DROP des contraintes à revoir.

--- sandboxes/depth/running/hvh/4.5.2/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.4.4.sql
+++ sandboxes/depth/running/hvh/4.5.2/joomla/easysdi/com_easysdi_core/src/administrator/sql/updates/sqlsrv/4.4.4.sql
@@ -4,17 +4,15 @@ ALTER TABLE [#__sdi_organism] ADD [fixed_fee_apply_vat] [smallint] NOT NULL DEFA
 ALTER TABLE [#__sdi_organism] DROP COLUMN [fixed_fee_ti];
 ALTER TABLE [#__sdi_organism] ADD [fixed_fee_te][decimal](6,2) DEFAULT 0;
 
-ALTER TABLE [#__sdi_pricing_order] DROP  CONSTRAINT [DF__sdi_sdi_p__cfg_o__40AF8DC9];
+
 ALTER TABLE [#__sdi_pricing_order] DROP COLUMN [cfg_overall_default_fee];
 ALTER TABLE [#__sdi_pricing_order] ADD [cfg_overall_default_fee_te][decimal](19,2) NOT NULL DEFAULT 0 ;
 ALTER TABLE [#__sdi_pricing_order] ADD  [cfg_fee_apply_vat] [smallint] NOT NULL DEFAULT 1 ;
 
-ALTER TABLE [#__sdi_pricing_order_supplier] DROP  CONSTRAINT [DF__sdi_sdi_p__cfg_f__44801EAD];
 ALTER TABLE [#__sdi_pricing_order_supplier] DROP COLUMN [cfg_fixed_fee_ti];
 ALTER TABLE [#__sdi_pricing_order_supplier] ADD [cfg_fixed_fee_te] [decimal](19,2) NOT NULL DEFAULT 0;
 ALTER TABLE [#__sdi_pricing_order_supplier] ADD  [cfg_fixed_fee_apply_vat] [smallint] NOT NULL DEFAULT 1 ;
 
-ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] DROP CONSTRAINT [DF__sdi_sdi_p__cfg_f__4944D3CA];
 ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] DROP COLUMN [cfg_fixed_fee];
 ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ADD [cfg_fixed_fee_te] [decimal](19,2) NOT NULL DEFAULT 0;
 ALTER TABLE [#__sdi_pricing_order_supplier_product_profile] ADD [cfg_apply_vat] [smallint] NOT NULL DEFAULT 1;
