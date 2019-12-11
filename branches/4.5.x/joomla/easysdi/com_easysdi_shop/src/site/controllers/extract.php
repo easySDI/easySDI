c0b2125a5ee6bdd32a82b07aaedb81beedf67ea1
yblatti <yblatti@f71f33a3-9987-4f9c-9609-a93d63a967da>
2019-12-11 15:10:14 +0000

    [yblatti] Bump version to 4.5.2

--- branches/4.5.x/joomla/easysdi/com_easysdi_shop/src/site/controllers/extract.php
+++ branches/4.5.x/joomla/easysdi/com_easysdi_shop/src/site/controllers/extract.php
@@ -4,9 +4,9 @@
  * Extract - WebService designed to getOrders and setProduct
  * based on the old webservice (rest)
  * 
- * @version     4.5.1
+ * @version     4.5.2
  * @package     com_easysdi_shop
- * @copyright   Copyright (C) 2013-2018. All rights reserved.
+ * @copyright   Copyright (C) 2013-2019. All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
  * @author      EasySDI Community <contact@easysdi.org> - http://www.easysdi.org
  */
